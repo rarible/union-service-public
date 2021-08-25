fbafc7aea759ed655f1bde8326c6a4af441fe2e8
Artem Yakischik <83639804+yakischik-aa@users.noreply.github.com>
2021-08-25 13:42:06 +0400

    RPN-781 (#7)
    Activity model + Events model

---
+++ openapi/protocol-union-model/src/main/kotlin/com/rarible/protocol/union/dto/EthAddress.kt
@@ -0,0 +1,5 @@
+package com.rarible.protocol.union.dto
+
+data class EthAddress(
+    val value: String
+)
---
+++ openapi/protocol-union-model/src/main/kotlin/com/rarible/protocol/union/dto/ItemId.kt
@@ -0,0 +1,5 @@
+package com.rarible.protocol.union.dto
+
+data class ItemId(
+    val value: String
+)
