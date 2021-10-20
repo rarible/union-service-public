8c19085882b412552ecdc4b749750333f794621b
Artem Yakischik <a.yakischik@rarible.com>
2021-10-20 17:32:03 +0400

    Reverted accident code change

--- enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/service/EnrichmentItemService.kt
+++ enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/service/EnrichmentItemService.kt
@@ -72,7 +72,7 @@ class EnrichmentItemService(
         val bestBidOrder = async { enrichmentOrderService.fetchOrderIfDiffers(shortItem?.bestBidOrder, orders) }
         val meta = async {
             val itemId = shortItem?.id ?: ShortItemId(item!!.id)
-            enrichmentMetaService.enrichMeta(fetchedItem.await().meta, itemId)
+            enrichmentMetaService.enrichMeta(item?.meta, itemId)
         }
 
         val bestOrders = listOf(bestSellOrder, bestBidOrder)
