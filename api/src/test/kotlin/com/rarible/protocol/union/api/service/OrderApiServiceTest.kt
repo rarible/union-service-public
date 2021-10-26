5c9b387d71d00c051c5dfa07eb1bfe82c0db08c3
Artem Yakischik <83639804+yakischik@users.noreply.github.com>
2021-10-26 14:49:36 +0400

    RPN-1277 (#89)
    Bid/Sell orders by item request fix

--- api/src/test/kotlin/com/rarible/protocol/union/api/service/OrderApiServiceTest.kt
+++ api/src/test/kotlin/com/rarible/protocol/union/api/service/OrderApiServiceTest.kt
@@ -4,14 +4,11 @@ import com.rarible.protocol.union.core.continuation.page.Slice
 import com.rarible.protocol.union.core.service.OrderService
 import com.rarible.protocol.union.core.service.router.BlockchainRouter
 import com.rarible.protocol.union.dto.BlockchainDto
+import com.rarible.protocol.union.dto.EthErc20AssetTypeDto
 import com.rarible.protocol.union.dto.ItemIdDto
 import com.rarible.protocol.union.dto.OrderDto
 import com.rarible.protocol.union.dto.PlatformDto
-import com.rarible.protocol.union.enrichment.converter.ShortItemConverter
-import com.rarible.protocol.union.enrichment.converter.ShortOrderConverter
-import com.rarible.protocol.union.enrichment.model.ShortItemId
-import com.rarible.protocol.union.enrichment.service.EnrichmentItemService
-import com.rarible.protocol.union.enrichment.test.data.randomUnionItem
+import com.rarible.protocol.union.dto.UnionAddress
 import com.rarible.protocol.union.enrichment.util.bidCurrencyId
 import com.rarible.protocol.union.integration.ethereum.converter.EthOrderConverter
 import com.rarible.protocol.union.integration.ethereum.data.randomEthItemId
@@ -28,25 +25,23 @@ import org.junit.jupiter.api.Test
 
 class OrderApiServiceTest {
 
-    private val enrichmentItemService: EnrichmentItemService = mockk()
     private val router: BlockchainRouter<OrderService> = mockk()
     private val orderService: OrderService = mockk()
     private val ethOrderConverter = EthOrderConverter(CurrencyMock.currencyServiceMock)
     private val orderApiService = OrderApiService(
-        router,
-        enrichmentItemService
+        router
     )
 
     @BeforeEach
     fun beforeEach() {
-        clearMocks(enrichmentItemService, router, orderService)
+        clearMocks(router, orderService)
         every { router.getService(BlockchainDto.ETHEREUM) } returns orderService
     }
 
     @Test
-    fun `get best bids - no item found`() = runBlocking<Unit> {
+    fun `get best bids - no currencies found`() = runBlocking<Unit> {
         val itemId = randomEthItemId()
-        coEvery { enrichmentItemService.get(ShortItemId(itemId)) } returns null
+        coEvery { orderService.getBidCurrencies(itemId.token.value, itemId.tokenId.toString()) } returns emptyList()
         val result = getOrderBidsByItem(itemId, null, 10)
 
         assertThat(result.continuation).isNull()
@@ -54,9 +49,9 @@ class OrderApiServiceTest {
     }
 
     @Test
-    fun `get best sells - no item found`() = runBlocking<Unit> {
+    fun `get best sells - no currencies found`() = runBlocking<Unit> {
         val itemId = randomEthItemId()
-        coEvery { enrichmentItemService.get(ShortItemId(itemId)) } returns null
+        coEvery { orderService.getSellCurrencies(itemId.token.value, itemId.tokenId.toString()) } returns emptyList()
         val result = getSellOrdersByItem(itemId, null, 10)
 
         assertThat(result.continuation).isNull()
@@ -66,17 +61,13 @@ class OrderApiServiceTest {
     @Test
     fun `get best bids - completed not requested`() = runBlocking<Unit> {
         val ethItemId = randomEthItemId()
-        val ethItem = randomUnionItem(ethItemId)
+        val ethOrder = randomEthLegacyOrderDto(ethItemId)
+        val unionOrder = ethOrderConverter.convert(ethOrder, ethItemId.blockchain)
 
-        val order = randomEthLegacyOrderDto(ethItemId)
-        val unionOrder = ethOrderConverter.convert(order, ethItemId.blockchain)
-        val shortOrder = ShortOrderConverter.convert(unionOrder)
-        val shortItem = ShortItemConverter.convert(ethItem).copy(
-            bestBidOrder = shortOrder,
-            bestBidOrders = mapOf(unionOrder.bidCurrencyId to shortOrder)
-        )
+        coEvery {
+            orderService.getBidCurrencies(ethItemId.token.value, ethItemId.tokenId.toString())
+        } returns listOf(EthErc20AssetTypeDto(UnionAddress(ethItemId.blockchain, unionOrder.bidCurrencyId)))
 
-        coEvery { enrichmentItemService.get(ShortItemId(ethItemId)) } returns shortItem
         val result = getOrderBidsByItem(ethItemId, "${unionOrder.bidCurrencyId}:COMPLETED", 10)
 
         assertThat(result.continuation).isNull()
