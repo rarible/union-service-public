f87399f054caa67481603640bd91d1a6eda65692
Artem Yakischik <83639804+yakischik@users.noreply.github.com>
2021-11-01 15:43:35 +0400

    RPN-1308 BigDecima/BigInteger serialization as strings (#96)

--- openapi/protocol-union-model/src/main/kotlin/com/rarible/protocol/union/dto/UnionPrimitivesJacksonModule.kt
+++ openapi/protocol-union-model/src/main/kotlin/com/rarible/protocol/union/dto/UnionPrimitivesJacksonModule.kt
@@ -1,14 +1,20 @@
 package com.rarible.protocol.union.dto
 
 import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.databind.ser.std.NumberSerializer
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
 import com.rarible.protocol.union.dto.deserializer.UnionAddressDeserializer
 import com.rarible.protocol.union.dto.serializer.UnionAddressSerializer
+import java.math.BigInteger
 
 object UnionPrimitivesJacksonModule : SimpleModule() {
 
     init {
         addSerializer(UnionAddressSerializer)
         addDeserializer(UnionAddress::class.java, UnionAddressDeserializer)
+
+        addSerializer(NumberSerializer.bigDecimalAsStringSerializer())
+        addSerializer(ToStringSerializer(BigInteger::class.java))
     }
 
 }
