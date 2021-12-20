bcf859671ddd277e41fd0deb6ab64cf28bcce74c
Roman Novikov <mystdeim@gmail.com>
2021-12-20 21:22:48 +0300

    HOTFIX: log for tezos for activity event

--- integration/tezos/src/main/kotlin/com/rarible/protocol/union/integration/tezos/event/TezosActivityEventHandler.kt
+++ integration/tezos/src/main/kotlin/com/rarible/protocol/union/integration/tezos/event/TezosActivityEventHandler.kt
@@ -1,6 +1,7 @@
 package com.rarible.protocol.union.integration.tezos.event
 
 import com.rarible.core.apm.CaptureTransaction
+import com.rarible.protocol.tezos.api.ApiClient
 import com.rarible.protocol.tezos.dto.TezosActivitySafeDto
 import com.rarible.protocol.union.core.handler.AbstractBlockchainEventHandler
 import com.rarible.protocol.union.core.handler.IncomingEventHandler
@@ -16,14 +17,16 @@ open class TezosActivityEventHandler(
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
+    private val mapper = ApiClient.createDefaultObjectMapper()
+
     @CaptureTransaction("ActivityEvent#TEZOS")
     override suspend fun handle(event: TezosActivitySafeDto) {
         if (event.nftType != null) {
-            logger.debug("Received Tezos ({}) Order Activity event: {}", blockchain, event)
+            logger.info("Received Tezos ({}) Order Activity event: {}", blockchain, mapper.writeValueAsString(event))
             val unionEventDto = tezosActivityConverter.convert(event.nftType!!, blockchain)
             handler.onEvent(unionEventDto)
         } else if (event.orderType != null) {
-            logger.debug("Received Tezos ({}) Order Activity event: {}", blockchain, event)
+            logger.info("Received Tezos ({}) Order Activity event: {}", blockchain, mapper.writeValueAsString(event))
             val unionEventDto = tezosActivityConverter.convert(event.orderType!!, blockchain)
             handler.onEvent(unionEventDto)
         }
