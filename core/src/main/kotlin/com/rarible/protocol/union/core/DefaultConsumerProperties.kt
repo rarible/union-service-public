cb2277e76410f6e2ebbec151a8ffd636442afdea
Roman Novikov <mystdeim@gmail.com>
2021-12-28 17:25:05 +0300

    HOTFIX: make nullable for api

--- core/src/main/kotlin/com/rarible/protocol/union/core/DefaultConsumerProperties.kt
+++ core/src/main/kotlin/com/rarible/protocol/union/core/DefaultConsumerProperties.kt
@@ -1,7 +1,7 @@
 package com.rarible.protocol.union.core
 
 open class DefaultConsumerProperties(
-    val brokerReplicaSet: String,
+    val brokerReplicaSet: String?,
     val workers: Map<String, Int>,
     val username: String? = null,
     val password: String? = null
