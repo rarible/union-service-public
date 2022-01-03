0a3ce8645ed155445d699b1147663f6dcb35d940
sanufriev <s.anufriev@rarible.com>
2022-01-03 16:36:34 +0200

    Add logs

--- listener/src/main/kotlin/com/rarible/protocol/union/listener/job/RefreshItemJob.kt
+++ listener/src/main/kotlin/com/rarible/protocol/union/listener/job/RefreshItemJob.kt
@@ -4,6 +4,7 @@ import com.rarible.protocol.union.enrichment.model.ShortItemId
 import com.rarible.protocol.union.enrichment.repository.ItemRepository
 import com.rarible.protocol.union.enrichment.service.EnrichmentRefreshService
 import kotlinx.coroutines.flow.*
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
@@ -11,9 +12,14 @@ class RefreshItemJob(
     private val itemRepository: ItemRepository,
     private val refreshService: EnrichmentRefreshService
 ) {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     fun refreshForPlatform(platform: String, fromShortItemId: ShortItemId?): Flow<ShortItemId> {
         return itemRepository.findWithSellAndPlatform(platform, fromShortItemId)
-            .onEach { shortItem -> refreshService.reconcileItem(shortItem.id.toDto(), full = true) }
+            .onEach { shortItem ->
+                refreshService.reconcileItem(shortItem.id.toDto(), full = true)
+                logger.info("Item ${shortItem.id} was reconciled, bestSellOrder=${shortItem.bestSellOrder}")
+            }
             .map { shortItem -> shortItem.id }
     }
 }
