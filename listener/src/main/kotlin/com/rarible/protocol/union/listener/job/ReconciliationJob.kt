2ca168e6be78eae8ab1e224de3509298b0d5cace
Roman Novikov <mystdeim@gmail.com>
2022-01-14 14:03:57 +0300

    NOTFIX: ignore private orders

--- listener/src/main/kotlin/com/rarible/protocol/union/listener/job/ReconciliationJob.kt
+++ listener/src/main/kotlin/com/rarible/protocol/union/listener/job/ReconciliationJob.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import org.slf4j.LoggerFactory
@@ -62,7 +63,13 @@ class ReconciliationJob(
         }
 
         coroutineScope {
-            orders.asFlow().map { async { safeUpdate(it) } }
+            orders.asFlow()
+                .filter {
+                    val isPublic = it.taker == null
+                    if (!isPublic) logger.info("Ignore private order [{}]", it.id)
+                    isPublic
+                }
+                .map { async { safeUpdate(it) } }
                 .buffer(config.threadCount)
                 .map { it.await() }
                 .collect()
