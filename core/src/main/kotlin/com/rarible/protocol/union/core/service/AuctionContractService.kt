30c3aad0947a22e9a68429909556749f16afd2b7
Artem Yakischik <83639804+yakischik@users.noreply.github.com>
2022-02-03 17:22:12 +0400

    Logging added for Auction contracts config

--- core/src/main/kotlin/com/rarible/protocol/union/core/service/AuctionContractService.kt
+++ core/src/main/kotlin/com/rarible/protocol/union/core/service/AuctionContractService.kt
@@ -2,16 +2,27 @@ package com.rarible.protocol.union.core.service
 
 import com.rarible.protocol.union.core.DefaultBlockchainProperties
 import com.rarible.protocol.union.dto.BlockchainDto
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
 
 @Component
 class AuctionContractService(
     blockchainProperties: List<DefaultBlockchainProperties>
 ) {
 
+    private val logger = LoggerFactory.getLogger(AuctionContractService::class.java)
+
     private val auctionContracts = blockchainProperties.associateBy({ it.blockchain }, { HashSet(it.auctionContracts) })
 
-    // TODO should we use here blockchainGroup instead of blockchain?
+
+    @PostConstruct
+    fun log() {
+        auctionContracts.forEach {
+            logger.info("Found configured Auction contracts: {} : {}", it.key, it.value)
+        }
+    }
+
     suspend fun isAuctionContract(blockchain: BlockchainDto, contract: String): Boolean {
         return auctionContracts[blockchain]!!.contains(contract)
     }
