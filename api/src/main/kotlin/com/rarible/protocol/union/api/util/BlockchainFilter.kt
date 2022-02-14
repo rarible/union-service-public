528ba0d9f1bafddb2b4ca47a7d2d1e6d38ea81c2
Artem Yakischik <83639804+yakischik@users.noreply.github.com>
2022-02-14 17:39:43 +0400

    feat: 'blockchains' query param for user-related requests added (#167)

---
+++ api/src/main/kotlin/com/rarible/protocol/union/api/util/BlockchainFilter.kt
@@ -0,0 +1,24 @@
+package com.rarible.protocol.union.api.util
+
+import com.rarible.protocol.union.dto.BlockchainDto
+import com.rarible.protocol.union.dto.BlockchainGroupDto
+import com.rarible.protocol.union.dto.subchains
+
+class BlockchainFilter(
+    fromQuery: Collection<BlockchainDto>?
+) {
+
+    private val filter = fromQuery?.let { HashSet(it) }
+
+    fun exclude(group: BlockchainGroupDto): List<BlockchainDto> {
+        return exclude(group.subchains())
+    }
+
+    fun exclude(blockchains: List<BlockchainDto>): List<BlockchainDto> {
+        if (filter == null) {
+            return blockchains
+        }
+        return blockchains.filter { filter.contains(it) }
+    }
+
+}
