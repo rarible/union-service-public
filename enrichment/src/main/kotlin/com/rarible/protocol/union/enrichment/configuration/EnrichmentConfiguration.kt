224346fc237825b96515e79c05bef56c0ec9c716
Sergey Patrikeev <s.patrikeev@rarible.com>
2022-02-25 12:52:36 +0100

    fix: remove dependency onto unused 'cache' and 'lock-redis'

--- enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/configuration/EnrichmentConfiguration.kt
+++ enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/configuration/EnrichmentConfiguration.kt
@@ -1,19 +1,14 @@
 package com.rarible.protocol.union.enrichment.configuration
 
-import com.rarible.core.lockredis.EnableRaribleRedisLock
 import com.rarible.core.mongo.configuration.EnableRaribleMongo
 import com.rarible.protocol.union.core.CoreConfiguration
 import com.rarible.protocol.union.core.event.OutgoingItemEventListener
-import com.rarible.protocol.union.enrichment.meta.UnionMetaPackage
 import com.rarible.protocol.union.enrichment.repository.ItemRepository
-import com.rarible.protocol.union.enrichment.service.EnrichmentItemService
 import com.rarible.protocol.union.enrichment.service.EnrichmentServicePackage
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Import
 
 @EnableRaribleMongo
-@EnableRaribleRedisLock
 @Import(CoreConfiguration::class)
 @ComponentScan(
     basePackageClasses = [
