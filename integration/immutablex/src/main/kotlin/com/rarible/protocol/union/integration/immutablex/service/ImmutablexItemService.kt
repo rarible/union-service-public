e4ae2e485594fad9a7e059826d44a7fe276ff898
zolt <timochkinea@gmail.com>
2022-03-01 21:02:37 +0700

    FB-626 get item meta by id for immutablex

--- integration/immutablex/src/main/kotlin/com/rarible/protocol/union/integration/immutablex/service/ImmutablexItemService.kt
+++ integration/immutablex/src/main/kotlin/com/rarible/protocol/union/integration/immutablex/service/ImmutablexItemService.kt
@@ -1,32 +1,19 @@
 package com.rarible.protocol.union.integration.immutablex.service
 
-import com.rarible.core.common.nowMillis
-import com.rarible.protocol.union.core.converter.ContractAddressConverter
-import com.rarible.protocol.union.core.converter.UnionAddressConverter
 import com.rarible.protocol.union.core.model.UnionItem
 import com.rarible.protocol.union.core.model.UnionMeta
 import com.rarible.protocol.union.core.service.ItemService
 import com.rarible.protocol.union.core.service.router.AbstractBlockchainService
 import com.rarible.protocol.union.dto.BlockchainDto
-import com.rarible.protocol.union.dto.CollectionIdDto
-import com.rarible.protocol.union.dto.ItemIdDto
 import com.rarible.protocol.union.dto.RoyaltyDto
 import com.rarible.protocol.union.dto.continuation.page.Page
 import com.rarible.protocol.union.integration.immutablex.client.ImmutablexApiClient
 import com.rarible.protocol.union.integration.immutablex.converter.ImmutablexItemConverter
-import com.rarible.protocol.union.integration.immutablex.dto.ImmutablexAsset
-import kotlinx.coroutines.reactor.awaitSingle
-import org.springframework.http.MediaType
-import org.springframework.web.reactive.function.client.WebClient
-import java.math.BigDecimal
-import java.math.BigInteger
+import com.rarible.protocol.union.integration.immutablex.converter.ImmutablexItemMetaConverter
 
 class ImmutablexItemService(
     private val client: ImmutablexApiClient
 ): AbstractBlockchainService(BlockchainDto.IMMUTABLEX), ItemService {
-
-
-    private val zeroAccount = "0x0000000000000000000000000000000000000000"
 
     override suspend fun getAllItems(
         continuation: String?,
@@ -45,15 +32,17 @@ class ImmutablexItemService(
     }
 
     override suspend fun getItemRoyaltiesById(itemId: String): List<RoyaltyDto> {
-        TODO("Not yet implemented")
+        val asset = client.getAsset(itemId)
+        return ImmutablexItemConverter.convert(asset, blockchain).royalties
     }
 
     override suspend fun getItemMetaById(itemId: String): UnionMeta {
-        TODO("Not yet implemented")
+        val asset = client.getAsset(itemId)
+        return ImmutablexItemMetaConverter.convert(asset)
     }
 
     override suspend fun resetItemMeta(itemId: String) {
-        TODO("Not yet implemented")
+        /** do nothing*/
     }
 
     override suspend fun getItemsByCollection(
