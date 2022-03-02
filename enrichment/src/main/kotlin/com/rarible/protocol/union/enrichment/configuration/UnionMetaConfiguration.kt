43f799a1b3e983f8722e7fd3270f193d737e017c
Sergey Patrikeev <s.patrikeev@rarible.com>
2022-03-02 19:11:54 +0100

    Bump 'content-meta-loader' to 2.3.30: metrics for content/media type receiving.

--- enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/configuration/UnionMetaConfiguration.kt
+++ enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/configuration/UnionMetaConfiguration.kt
@@ -2,12 +2,14 @@ package com.rarible.protocol.union.enrichment.configuration
 
 import com.rarible.core.content.meta.loader.ContentMetaReceiver
 import com.rarible.core.content.meta.loader.ContentReceiver
+import com.rarible.core.content.meta.loader.ContentReceiverMetrics
 import com.rarible.core.content.meta.loader.KtorApacheClientContentReceiver
 import com.rarible.loader.cache.CacheLoaderService
 import com.rarible.loader.cache.configuration.EnableRaribleCacheLoader
 import com.rarible.protocol.union.core.model.UnionMeta
 import com.rarible.protocol.union.enrichment.meta.UnionMetaCacheLoader
 import com.rarible.protocol.union.enrichment.meta.UnionMetaPackage
+import io.micrometer.core.instrument.MeterRegistry
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
@@ -31,10 +33,12 @@ class UnionMetaConfiguration {
     @Bean
     fun contentMetaReceiver(
         contentReceiver: ContentReceiver,
-        unionMetaProperties: UnionMetaProperties
+        unionMetaProperties: UnionMetaProperties,
+        meterRegistry: MeterRegistry
     ): ContentMetaReceiver = ContentMetaReceiver(
         contentReceiver = contentReceiver,
-        maxBytes = unionMetaProperties.mediaFetchMaxSize.toInt()
+        maxBytes = unionMetaProperties.mediaFetchMaxSize.toInt(),
+        contentReceiverMetrics = ContentReceiverMetrics(meterRegistry)
     )
 
     @Bean
