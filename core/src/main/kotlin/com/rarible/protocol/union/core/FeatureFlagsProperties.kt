f3ec4816db2a9ee496b79ca96183e246004dcf65
Artem Yakischik <a.yakischik@rarible.com>
2022-03-23 19:16:58 +0400

    Properties fix

--- core/src/main/kotlin/com/rarible/protocol/union/core/FeatureFlagsProperties.kt
+++ core/src/main/kotlin/com/rarible/protocol/union/core/FeatureFlagsProperties.kt
@@ -1,8 +1,10 @@
 package com.rarible.protocol.union.core
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConfigurationProperties("common.feature-flags")
+@ConstructorBinding
 data class FeatureFlagsProperties(
     val enableRevertedActivityEvents: Boolean = false,
     val enableNotificationOnCollectionOrders: Boolean = true
