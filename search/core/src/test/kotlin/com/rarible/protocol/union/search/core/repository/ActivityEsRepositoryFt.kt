c5d4bbad23f3ce6c6d8eaecf2997c9f3ec3961d8
Andrey Atapin <a.atapin@gmail.com>
2022-03-25 12:15:24 +0500

    ALPHA-295 activity index model and storage

---
+++ search/core/src/test/kotlin/com/rarible/protocol/union/search/core/repository/ActivityEsRepositoryFt.kt
@@ -0,0 +1,44 @@
+package com.rarible.protocol.union.search.core.repository
+
+import com.rarible.protocol.union.dto.ActivityTypeDto
+import com.rarible.protocol.union.dto.BlockchainDto
+import com.rarible.protocol.union.search.core.config.SearchConfiguration
+import com.rarible.protocol.union.search.test.AbstractIntegrationTest
+import com.rarible.protocol.union.search.test.IntegrationTest
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.test.context.ContextConfiguration
+import java.time.Instant
+
+@IntegrationTest
+@EnableAutoConfiguration
+@ContextConfiguration(classes = [SearchConfiguration::class])
+internal class ActivityEsRepositoryFt {
+
+    @Autowired
+    protected lateinit var activityEsRepository: ActivityEsRepository
+
+    @Test
+    fun `should init`(): Unit = runBlocking {
+        val activity = ElasticActivity(
+            "1234", Instant.MIN, 1, 0, BlockchainDto.ETHEREUM, ActivityTypeDto.BURN,
+            ElasticActivity.User(
+                "0x01", null
+            ),
+            ElasticActivity.Collection(
+                "0x02", null
+            ),
+            ElasticActivity.Item(
+                "0x03", null
+            )
+        )
+
+        val id = activityEsRepository.putDocument(activity)
+        val found = activityEsRepository.getById(id)
+
+        Assertions.assertThat(found).isEqualTo(activity)
+    }
+}
---
+++ search/core/src/test/kotlin/com/rarible/protocol/union/search/test/TestSearchConfiguration.kt
@@ -0,0 +1,10 @@
+package com.rarible.protocol.union.search.test
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Lazy
+
+@Lazy
+@Configuration
+class TestSearchConfiguration {
+
+}
