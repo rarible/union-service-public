157de03df34563040fce94e2a6702bbe7e13cd7c
Denis Kilchichakov <kdenis87@gmail.com>
2022-03-25 13:20:16 +0300

    ALPHA-315: Filters to query activities in Elastic

---
+++ search/core/src/main/kotlin/com/rarible/protocol/union/search/core/filter/ActivityFilters.kt
@@ -0,0 +1,47 @@
+package com.rarible.protocol.union.search.core.filter
+
+import com.rarible.protocol.union.dto.ActivityTypeDto
+import com.rarible.protocol.union.dto.BlockchainDto
+import java.time.Instant
+
+/**
+ * ALL conditions (that aren't null/empty) must be true to pass the filter
+ */
+data class ElasticActivityQueryGenericFilter(
+    val blockchains: Set<BlockchainDto>,
+    val activityTypes: Set<ActivityTypeDto>,
+    val anyUsers: Set<String>,
+    val makers: Set<String>,
+    val takers: Set<String>,
+    val anyCollections: Set<String>,
+    val makeCollections: Set<String>,
+    val takeCollections: Set<String>,
+    val anyItems: Set<String>,
+    val makeItems: Set<String>,
+    val takeItems: Set<String>,
+    val from: Instant?,
+    val to: Instant?,
+    val continuation: String?,
+)
+
+/**
+ * ALL conditions (that aren't null/empty) must be true to pass the filter,
+ * also additional conditions, specific to each ActivityType, are applied
+ */
+data class ElasticActivityQueryPerTypeFilter(
+    val blockchains: Set<BlockchainDto>,
+    val from: Instant?,
+    val to: Instant?,
+    val continuation: String?,
+    val filters: Map<ActivityTypeDto, Filter>,
+) {
+
+    data class Filter(
+        val makers: Set<String>,
+        val takers: Set<String>,
+        val makeCollection: String?,
+        val takeCollection: String?,
+        val makeItem: String?,
+        val takeItem: String?,
+    )
+}
