fe9d7573529b651132e03f26fd4d4276fad00b3d
Andrey Atapin <a.atapin@gmail.com>
2022-03-25 12:15:24 +0500

    ALPHA-295 activity index model and storage

---
+++ search/indexer/src/main/kotlin/com/rarible/protocol/union/search/indexer/UnionSearchIndexerApp.kt
@@ -0,0 +1,12 @@
+package com.rarible.protocol.union.search.indexer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class UnionSearchIndexerApp
+
+fun main(args: Array<String>): Unit {
+    runApplication<UnionSearchIndexerApp>(*args)
+}
