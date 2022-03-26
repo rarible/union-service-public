822d5ac59db0b5d808b9f15a65de1591afae933e
Sergey Ratushnyak <rtserg@yandex.ru>
2022-03-26 12:19:11 +0300

    fix: ALPHA-302 * added new engine for ktor - CIO * the settings of http client are in the configuration file

--- enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/configuration/UnionMetaConfiguration.kt
+++ enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/configuration/UnionMetaConfiguration.kt
@@ -4,16 +4,22 @@ import com.rarible.core.content.meta.loader.ContentMetaReceiver
 import com.rarible.core.content.meta.loader.ContentReceiver
 import com.rarible.core.content.meta.loader.ContentReceiverMetrics
 import com.rarible.core.content.meta.loader.KtorApacheClientContentReceiver
+import com.rarible.core.content.meta.loader.KtorCioClientContentReceiver
 import com.rarible.loader.cache.CacheLoaderService
 import com.rarible.loader.cache.configuration.EnableRaribleCacheLoader
 import com.rarible.protocol.union.core.model.UnionMeta
 import com.rarible.protocol.union.enrichment.meta.UnionMetaCacheLoader
 import com.rarible.protocol.union.enrichment.meta.UnionMetaPackage
 import io.micrometer.core.instrument.MeterRegistry
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
+
+const val CONTENT_RECEIVER_KTOR_APACHE = "ktor-apache"
+const val CONTENT_RECEIVER_KTOR_CIO = "ktor-cio"
 
 @EnableRaribleCacheLoader
 @EnableConfigurationProperties(UnionMetaProperties::class)
@@ -23,13 +29,32 @@ import org.springframework.context.annotation.ComponentScan
     ]
 )
 class UnionMetaConfiguration {
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(UnionMetaConfiguration::class.java)
+    }
+
     @Bean
     fun contentReceiver(
         unionMetaProperties: UnionMetaProperties
-    ): ContentReceiver = KtorApacheClientContentReceiver(
-        timeout = unionMetaProperties.mediaFetchTimeout,
-        threadsCount = unionMetaProperties.ktorApacheHttpClientThreadCount
-    )
+    ): ContentReceiver = when (unionMetaProperties.httpClient.name) {
+        CONTENT_RECEIVER_KTOR_APACHE ->
+            KtorApacheClientContentReceiver(
+                timeout = unionMetaProperties.httpClient.timeOut,
+                threadsCount = unionMetaProperties.httpClient.threadCount,
+                totalConnection = unionMetaProperties.httpClient.totalConnection
+            )
+
+        CONTENT_RECEIVER_KTOR_CIO ->
+            KtorCioClientContentReceiver(
+                timeout = unionMetaProperties.httpClient.timeOut,
+                threadsCount = unionMetaProperties.httpClient.threadCount,
+                totalConnection = unionMetaProperties.httpClient.totalConnection
+            )
+        else ->{
+            logger.error("Wrong http client name, ktor-apache will be used with default values")
+            KtorApacheClientContentReceiver(unionMetaProperties.httpClient.threadCount)
+        }
+    }
 
     @Bean
     fun contentMetaReceiver(
@@ -49,4 +74,5 @@ class UnionMetaConfiguration {
     ): CacheLoaderService<UnionMeta> =
         @Suppress("UNCHECKED_CAST")
         (cacheLoaderServices.find { it.type == UnionMetaCacheLoader.TYPE } as CacheLoaderService<UnionMeta>)
+
 }
--- enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/configuration/UnionMetaProperties.kt
+++ enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/configuration/UnionMetaProperties.kt
@@ -2,14 +2,19 @@ package com.rarible.protocol.union.enrichment.configuration
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
-import java.time.Duration
 
 @ConstructorBinding
 @ConfigurationProperties("meta")
 data class UnionMetaProperties(
     val ipfsGateway: String,
-    val mediaFetchTimeout: Int,
     val mediaFetchMaxSize: Long,
     val openSeaProxyUrl: String,
-    val ktorApacheHttpClientThreadCount: Int = 8
-)
+    val httpClient: HttpClient
+){
+    class HttpClient(
+        val name: String = "ktor-apache",
+        val threadCount: Int = 8,
+        val timeOut: Int = 5000,
+        val totalConnection: Int = 500
+    )
+}
