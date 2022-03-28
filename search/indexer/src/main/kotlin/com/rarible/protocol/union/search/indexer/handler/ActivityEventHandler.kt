0814e8947f2f703597b3567cdef0b9e24742a0c6
Denis Kilchichakov <kdenis87@gmail.com>
2022-03-28 15:35:02 +0300

    ALPHA-317: listen to union activity topic in search

--- search/indexer/src/main/kotlin/com/rarible/protocol/union/search/indexer/handler/ActivityEventHandler.kt
+++ search/indexer/src/main/kotlin/com/rarible/protocol/union/search/indexer/handler/ActivityEventHandler.kt
@@ -3,12 +3,15 @@ package com.rarible.protocol.union.search.indexer.handler
 import com.rarible.core.daemon.sequential.ConsumerEventHandler
 import com.rarible.protocol.union.dto.ActivityDto
 import com.rarible.protocol.union.search.core.converter.ElasticActivityConverter
+import com.rarible.protocol.union.search.core.repository.ActivityEsRepository
+import kotlinx.coroutines.reactive.awaitFirst
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class ActivityEventHandler(
-    private val converter: ElasticActivityConverter
+    private val converter: ElasticActivityConverter,
+    private val repository: ActivityEsRepository,
 ): ConsumerEventHandler<ActivityDto> {
 
     private val logger = LoggerFactory.getLogger(javaClass)
@@ -16,7 +19,7 @@ class ActivityEventHandler(
     override suspend fun handle(event: ActivityDto) {
         logger.debug("Handling ActivityDto id = ${event.id}")
         val activity = converter.convert(event)
-        // TODO save to ElasticSearch
+        repository.save(activity).awaitFirst()
         logger.debug("Handled ActivityDto id = ${event.id}")
     }
 }
