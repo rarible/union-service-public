5a2225b755a393a6a3d69adf67885cf79b618190
Andrey Atapin <a.atapin@gmail.com>
2022-03-28 17:05:33 +0500

    ALPHA-295 compilation fix

--- search/core/src/main/kotlin/com/rarible/protocol/union/search/core/config/SearchConfiguration.kt
+++ search/core/src/main/kotlin/com/rarible/protocol/union/search/core/config/SearchConfiguration.kt
@@ -1,20 +1,16 @@
 package com.rarible.protocol.union.search.core.config
 
-import co.elastic.clients.elasticsearch.ElasticsearchAsyncClient
-import co.elastic.clients.json.jackson.JacksonJsonpMapper
-import co.elastic.clients.transport.rest_client.RestClientTransport
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.rarible.protocol.union.search.core.repository.ActivityEsRepository
-import org.apache.http.HttpHost
-import org.elasticsearch.client.Node
-import org.elasticsearch.client.RestClient
+import org.elasticsearch.client.RestHighLevelClient
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.elasticsearch.client.ClientConfiguration
+import org.springframework.data.elasticsearch.client.RestClients
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration
+import org.springframework.data.elasticsearch.repository.config.EnableReactiveElasticsearchRepositories
+
 
 @Configuration
-//@EnableConfigurationProperties(SearchProperties::class)
+@EnableReactiveElasticsearchRepositories
 class SearchConfiguration(
     @Value("\${elasticsearch.api-nodes}") private val elasticsearchHost: String
 ): AbstractElasticsearchConfiguration() {
