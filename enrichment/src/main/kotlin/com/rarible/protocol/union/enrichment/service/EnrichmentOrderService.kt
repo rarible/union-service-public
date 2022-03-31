60543b6c78c655cde457d11af472e175e45ff234
Sergey Ratushnyak <rtserg@yandex.ru>
2022-03-31 12:44:05 +0300

    fix ALPHA-336 Do not use Orders with CollectionAssetType in Items.bestBidOrder

--- enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/service/EnrichmentOrderService.kt
+++ enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/service/EnrichmentOrderService.kt
@@ -9,6 +9,7 @@ import com.rarible.protocol.union.dto.OrderIdDto
 import com.rarible.protocol.union.dto.OrderStatusDto
 import com.rarible.protocol.union.dto.PlatformDto
 import com.rarible.protocol.union.dto.continuation.page.Slice
+import com.rarible.protocol.union.dto.ext
 import com.rarible.protocol.union.enrichment.model.ShortItemId
 import com.rarible.protocol.union.enrichment.model.ShortOrder
 import com.rarible.protocol.union.enrichment.model.ShortOwnershipId
@@ -49,7 +50,7 @@ class EnrichmentOrderService(
 
     suspend fun getBestSell(id: ShortItemId, currencyId: String): OrderDto? {
         val now = nowMillis()
-        val result = withPreferredRariblePlatform(id) { platform, continuation, size ->
+        val result = withPreferredRariblePlatform(id, OrderFilters.ITEM) { platform, continuation, size ->
             orderServiceRouter.getService(id.blockchain).getSellOrdersByItem(
                 platform,
                 id.toDto().value,
@@ -91,7 +92,7 @@ class EnrichmentOrderService(
 
     suspend fun getBestBid(id: ShortItemId, currencyId: String): OrderDto? {
         val now = nowMillis()
-        val result = withPreferredRariblePlatform(id) { platform, continuation, size ->
+        val result = withPreferredRariblePlatform(id, OrderFilters.ITEM) { platform, continuation, size ->
             orderServiceRouter.getService(id.blockchain).getOrderBidsByItem(
                 platform,
                 id.toDto().value,
@@ -114,15 +115,16 @@ class EnrichmentOrderService(
 
     private suspend fun withPreferredRariblePlatform(
         id: Any,
+        filter: OrderFilters = OrderFilters.ALL,
         clientCall: suspend (platform: PlatformDto?, continuation: String?, size: Int) -> Slice<OrderDto>
     ): OrderDto? {
-        val bestOfAll = ignoreFilledTaker(id, clientCall, null)
+        val bestOfAll = ignoreFilledTaker(id, clientCall, null, filter = filter)
         logger.debug("Found best order from ALL platforms: [{}]", bestOfAll)
         if (bestOfAll == null || bestOfAll.platform == PlatformDto.RARIBLE) {
             return bestOfAll
         }
         logger.debug("Order [{}] is not a preferred platform order, checking preferred platform...", bestOfAll)
-        val preferredPlatformBestOrder = ignoreFilledTaker(id, clientCall, PlatformDto.RARIBLE)
+        val preferredPlatformBestOrder = ignoreFilledTaker(id, clientCall, PlatformDto.RARIBLE, filter = filter)
         logger.debug("Checked preferred platform for best order: [{}]")
         return preferredPlatformBestOrder ?: bestOfAll
     }
@@ -130,7 +132,8 @@ class EnrichmentOrderService(
     suspend fun ignoreFilledTaker(
         id: Any,
         clientCall: suspend (platform: PlatformDto?, continuation: String?, size: Int) -> Slice<OrderDto>,
-        platform: PlatformDto?
+        platform: PlatformDto?,
+        filter: OrderFilters
     ): OrderDto? {
         var order: OrderDto?
         var continuation: String? = null
@@ -141,9 +144,9 @@ class EnrichmentOrderService(
 
         do {
             val slice = clientCall(platform, continuation, size)
-            order = slice.entities.firstOrNull()?.takeIf {
+            order = slice.entities.firstOrNull {
                 // TODO important! may affect performance
-                BestOrderValidator.isValid(it)
+                BestOrderValidator.isValid(it) && orderFilter(it, filter)
             }
             continuation = slice.continuation
             attempts++
@@ -163,11 +166,21 @@ class EnrichmentOrderService(
         return order
     }
 
+    private fun orderFilter(order: OrderDto, filter: OrderFilters): Boolean {
+        return when (filter) {
+            OrderFilters.COLLECTION -> order.make.type.ext.isCollection
+            OrderFilters.ITEM -> !order.make.type.ext.isCollection
+            OrderFilters.ALL -> true
+        }
+    }
+
     companion object {
         private const val MAX_ATTEMPTS = 50
         private const val WARN_ATTEMPTS = 5
 
         private const val SWITCH_TO_BATCH_ATTEMPTS = 2
         private const val ORDER_BATCH = 10
+
+        enum class OrderFilters { ALL, COLLECTION, ITEM }
     }
 }
