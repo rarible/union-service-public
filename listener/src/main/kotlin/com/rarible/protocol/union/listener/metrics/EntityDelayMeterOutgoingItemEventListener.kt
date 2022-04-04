a38089e1f4892bb96c7a0ec945fccf5c5b506f0d
sanufriev <s.anufriev@rarible.com>
2022-04-04 22:29:24 +0700

    feat: ALPHA-318 Add entity delay metrics

---
+++ listener/src/main/kotlin/com/rarible/protocol/union/listener/metrics/EntityDelayMeterOutgoingItemEventListener.kt
@@ -0,0 +1,29 @@
+package com.rarible.protocol.union.listener.metrics
+
+import com.rarible.protocol.union.core.event.OutgoingEventListener
+import com.rarible.protocol.union.core.model.CompositeRegisteredTimer
+import com.rarible.protocol.union.dto.BlockchainDto
+import java.time.Clock
+import java.time.Duration
+import java.time.Instant
+
+abstract class EntityDelayMeterOutgoingItemEventListener<T>(
+    private val clock: Clock,
+    private val timer: CompositeRegisteredTimer,
+) : OutgoingEventListener<T> {
+
+    override suspend fun onEvent(event: T) {
+        val blockchain = extractBlockchain(event)
+        val eventTimestamp = extractLastUpdateAt(event)
+
+        val now = clock.instant()
+        val eventDelay = now.toEpochMilli() - eventTimestamp.toEpochMilli()
+
+        timer.record(Duration.ofMillis(eventDelay), blockchain)
+    }
+
+    protected abstract fun extractLastUpdateAt(event: T): Instant
+
+    protected abstract fun extractBlockchain(event: T): BlockchainDto
+}
+
