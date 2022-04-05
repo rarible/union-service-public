189e07976b771269897597fd7defac2b939a0891
Denis Kilchichakov <augur@users.noreply.github.com>
2022-04-05 09:13:39 +0300

    ALPHA-331 Build filters for activity queries to Elastic (#216)
    * ALPHA-331: Build filters for activity queries to Elastic
    
    * ALPHA-331: review note fix
    
    * ALPHA-331: native query builder service added

---
+++ search/core/src/main/kotlin/com/rarible/protocol/union/search/core/service/query/QuerySortService.kt
@@ -0,0 +1,8 @@
+package com.rarible.protocol.union.search.core.service.query
+
+import org.springframework.stereotype.Service
+
+@Service
+class QuerySortService {
+    // TODO ALPHA-349: Apply sort to query to ElasticSearch
+}
