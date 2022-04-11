9c4279e255993e3ee3ffb678214e0c5e423ec86c
Denis Kilchichakov <kdenis87@gmail.com>
2022-04-11 12:38:36 +0300

    fix build

--- api/src/main/kotlin/com/rarible/protocol/union/api/UnionApiApplication.kt
+++ api/src/main/kotlin/com/rarible/protocol/union/api/UnionApiApplication.kt
@@ -2,7 +2,6 @@ package com.rarible.protocol.union.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.reactive.config.EnableWebFlux
 
 @SpringBootApplication
 class UnionApiApplication
