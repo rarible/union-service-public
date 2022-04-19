12b0854d2ec74e9a7409ca689773ba67fc292841
Dmitrii <d.zhalnin@rarible.com>
2022-04-19 18:34:07 +0700

    ALPHA-425 Fix dto, add mapping file

--- core/src/main/kotlin/com/rarible/protocol/union/core/converter/EsOwnershipConverter.kt
+++ core/src/main/kotlin/com/rarible/protocol/union/core/converter/EsOwnershipConverter.kt
@@ -7,16 +7,9 @@ object EsOwnershipConverter {
     fun convert(source: OwnershipDto): EsOwnership = EsOwnership(
         ownershipId = source.id.fullId(),
         blockchain = source.blockchain,
-        token = source.contract?.fullId(),
-        tokenId = source.tokenId?.toString(),
         itemId = source.itemId?.fullId(),
         collection = source.collection?.fullId(),
-        owner = source.owner.fullId(),
         value = source.value.toString(),
         date = source.createdAt,
-        price = source.bestSellOrder?.takePrice?.toString(),
-        priceUsd = source.bestSellOrder?.takePriceUsd?.toString(),
-        auctionEndDate = source.auction?.endTime,
-        orderSource = source.bestSellOrder?.platform?.name,
     )
 }
