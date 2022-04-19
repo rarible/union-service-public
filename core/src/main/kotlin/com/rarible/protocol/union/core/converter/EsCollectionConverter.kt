eb91f3879d0332c0cef7e2befa92466e68875302
Evgeny Timochkin <timochkinea@gmail.com>
2022-04-19 13:49:47 +0700

    ALPHA-421 collection mapping for elastic, model and converter (#257)
    * ALPHA-421 collection mapping for elastic, model and converter
    
    * ALPHA-421 change UnionCollection to CollectionDto
    
    * ALPHA-421 fix converter test
    
    * ALPHA-421 fix mappings definition

---
+++ core/src/main/kotlin/com/rarible/protocol/union/core/converter/EsCollectionConverter.kt
@@ -0,0 +1,25 @@
+package com.rarible.protocol.union.core.converter
+
+import com.rarible.protocol.union.core.model.EsCollection
+import com.rarible.protocol.union.dto.CollectionDto
+
+object EsCollectionConverter {
+
+    fun convert(collection: CollectionDto): EsCollection {
+        return EsCollection(
+            collectionId = collection.id.fullId(),
+            type = collection.type.name,
+            name = collection.name,
+            symbol = collection.symbol,
+            owner = collection.owner?.fullId(),
+            parent = collection.parent?.fullId(),
+            meta = collection.meta?.let {
+                EsCollection.CollectionMeta(
+                    name = it.name,
+                    description = it.description,
+                    feeRecipient = it.feeRecipient?.fullId()
+                )
+            }
+        )
+    }
+}
