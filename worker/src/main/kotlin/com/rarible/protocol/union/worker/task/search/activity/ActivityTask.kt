868de13eb769932385d3b3e4ccd9ef421ec6b4b1
Andrey Atapin <a.atapin@gmail.com>
2022-04-25 11:48:02 +0500

    ALPHA-440 manage reindexer tasks

--- worker/src/main/kotlin/com/rarible/protocol/union/worker/task/search/activity/ActivityTask.kt
+++ worker/src/main/kotlin/com/rarible/protocol/union/worker/task/search/activity/ActivityTask.kt
@@ -1,28 +1,25 @@
-package com.rarible.protocol.union.worker.task
+package com.rarible.protocol.union.worker.task.search.activity
 
 import com.rarible.core.task.RunTask
 import com.rarible.core.task.TaskHandler
 import com.rarible.protocol.union.api.client.ActivityControllerApi
-import com.rarible.protocol.union.dto.ActivitySortDto
 import com.rarible.protocol.union.core.converter.EsActivityConverter
+import com.rarible.protocol.union.dto.ActivitySortDto
+import com.rarible.protocol.union.enrichment.repository.search.EsActivityRepository
 import com.rarible.protocol.union.worker.config.SearchReindexerConfiguration
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.reactive.awaitFirst
-import kotlinx.coroutines.reactive.awaitSingle
-import org.springframework.data.elasticsearch.core.ReactiveElasticsearchOperations
 
 class ActivityTask(
     private val config: SearchReindexerConfiguration,
     private val activityClient: ActivityControllerApi,
-    private val esOperations: ReactiveElasticsearchOperations,
+    private val esActivityRepository: EsActivityRepository,
     private val converter: EsActivityConverter
-): TaskHandler<String> {
+): TaskHandler<ActivityTaskState> {
     override val type: String
         get() = ACTIVITY_REINDEX
-
-    private val tasks = config.properties.activityTasks.associateBy { it.taskParam() }
 
     override fun getAutorunParams(): List<RunTask> {
         return config.properties.activityTasks.map {
@@ -38,32 +35,31 @@ class ActivityTask(
      * from - cursor
      * param looks like ACTIVITY_ETHEREUM_LIST
      */
-    override fun runLongTask(from: String?, param: String): Flow<String> {
-        val task = tasks[param]
-        return if(task == null || from == "") {
+    override fun runLongTask(from: ActivityTaskState?, param: String): Flow<ActivityTaskState> {
+        return if(from == null) {
             emptyFlow()
         } else {
             flow {
                 val res = activityClient.getAllActivities(
-                    listOf(task.type),
-                    listOf(task.blockchainDto),
-                    from,
-                    from,
+                    listOf(from.activityType),
+                    listOf(from.blockchain),
+                    from.cursor,
+                    from.cursor,
                     PAGE_SIZE,
                     ActivitySortDto.EARLIEST_FIRST
                 ).awaitFirst()
 
-                esOperations.save(
+                esActivityRepository.saveAll(
                     res.activities.mapNotNull(converter::convert)
-                ).awaitSingle()
+                )
 
-                emit(res.continuation ?: "")
+                emit(from.next(res.cursor))
             }
         }
     }
 
     companion object {
-        private const val ACTIVITY_REINDEX = "ACTIVITY_REINDEX"
+        const val ACTIVITY_REINDEX = "ACTIVITY_REINDEX"
         const val PAGE_SIZE = 1000
     }
 }
