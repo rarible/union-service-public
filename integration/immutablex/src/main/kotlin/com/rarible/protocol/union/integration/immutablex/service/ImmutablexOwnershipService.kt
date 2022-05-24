4945f24ca9116984b388f9abc19cb132278ba4dc
zolt <timochkinea@gmail.com>
2022-05-24 15:21:44 +0700

    FB-663 immutablex: add creators filling for ownership API

--- integration/immutablex/src/main/kotlin/com/rarible/protocol/union/integration/immutablex/service/ImmutablexOwnershipService.kt
+++ integration/immutablex/src/main/kotlin/com/rarible/protocol/union/integration/immutablex/service/ImmutablexOwnershipService.kt
@@ -5,8 +5,11 @@ import com.rarible.protocol.union.core.service.OwnershipService
 import com.rarible.protocol.union.core.service.router.AbstractBlockchainService
 import com.rarible.protocol.union.dto.BlockchainDto
 import com.rarible.protocol.union.dto.CollectionIdDto
+import com.rarible.protocol.union.dto.CreatorDto
 import com.rarible.protocol.union.dto.OwnershipIdDto
+import com.rarible.protocol.union.dto.UnionAddress
 import com.rarible.protocol.union.dto.continuation.page.Page
+import com.rarible.protocol.union.dto.group
 import com.rarible.protocol.union.integration.immutablex.client.ImmutablexApiClient
 import com.rarible.protocol.union.integration.immutablex.dto.ImmutablexAsset
 import com.rarible.protocol.union.integration.immutablex.dto.ImmutablexAssetsPage
@@ -48,18 +51,20 @@ class ImmutablexOwnershipService(
         } else asset
     }
 
-    private fun convert(asset: ImmutablexAsset): UnionOwnership {
+    private suspend fun convert(asset: ImmutablexAsset): UnionOwnership {
+        val creator = client.getMints(pageSize = 1, itemId = asset.itemId).result.first().user
         return UnionOwnership(
             id = OwnershipIdDto(BlockchainDto.IMMUTABLEX, asset.tokenAddress, asset.tokenId(), asset.user!!),
             collection = CollectionIdDto(BlockchainDto.IMMUTABLEX, asset.tokenAddress),
             value = BigInteger.ONE,
             lazyValue = BigInteger.ZERO,
             createdAt = asset.createdAt!!,
-            lastUpdatedAt = asset.updatedAt
+            lastUpdatedAt = asset.updatedAt,
+            creators = listOf(CreatorDto(account = UnionAddress(blockchain.group(), creator), 1))
         )
     }
 
-    private fun convert(page: ImmutablexAssetsPage): Page<UnionOwnership> {
+    private suspend fun convert(page: ImmutablexAssetsPage): Page<UnionOwnership> {
         return Page(
             0L,
             page.cursor,
