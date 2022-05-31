c8c4ebffaf22922a63eccf78f2d77bfa494438f1
Denis Kilchichakov <kdenis87@gmail.com>
2022-05-31 15:24:47 +0300

    ALPHA-558: config to indexer (final х2)

--- indexer/src/main/kotlin/com/rarible/protocol/union/search/indexer/config/UnionIndexerConfiguration.kt
+++ indexer/src/main/kotlin/com/rarible/protocol/union/search/indexer/config/UnionIndexerConfiguration.kt
@@ -1,8 +1,15 @@
 package com.rarible.protocol.union.search.indexer.config
 
 import com.rarible.protocol.union.enrichment.configuration.EnrichmentApiConfiguration
+import com.rarible.protocol.union.enrichment.configuration.EnrichmentConfiguration
 import com.rarible.protocol.union.enrichment.configuration.SearchConfiguration
 import com.rarible.protocol.union.enrichment.repository.search.EsActivityRepository
+import com.rarible.protocol.union.integration.ethereum.EthereumApiConfiguration
+import com.rarible.protocol.union.integration.ethereum.PolygonApiConfiguration
+import com.rarible.protocol.union.integration.flow.FlowApiConfiguration
+import com.rarible.protocol.union.integration.immutablex.ImmutablexApiConfiguration
+import com.rarible.protocol.union.integration.solana.SolanaApiConfiguration
+import com.rarible.protocol.union.integration.tezos.TezosApiConfiguration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
@@ -13,7 +20,13 @@ import org.springframework.context.annotation.Import
 @ComponentScan(basePackageClasses = [EsActivityRepository::class])
 @Import(
     value = [
-        EnrichmentApiConfiguration::class,
+        EnrichmentConfiguration::class,
+        EthereumApiConfiguration::class,
+        PolygonApiConfiguration::class,
+        FlowApiConfiguration::class,
+        TezosApiConfiguration::class,
+        SolanaApiConfiguration::class,
+        ImmutablexApiConfiguration::class,
         SearchConfiguration::class
     ]
 )
