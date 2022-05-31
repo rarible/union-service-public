e2879b3d42dc3f02a3f115258a306180c2b67fe6
Denis Kilchichakov <kdenis87@gmail.com>
2022-05-31 14:18:53 +0300

    ALPHA-558: config to indexer

--- indexer/src/main/kotlin/com/rarible/protocol/union/search/indexer/config/UnionIndexerConfiguration.kt
+++ indexer/src/main/kotlin/com/rarible/protocol/union/search/indexer/config/UnionIndexerConfiguration.kt
@@ -1,6 +1,7 @@
 package com.rarible.protocol.union.search.indexer.config
 
 import com.rarible.protocol.union.core.CoreConfiguration
+import com.rarible.protocol.union.enrichment.configuration.EnrichmentApiConfiguration
 import com.rarible.protocol.union.enrichment.configuration.SearchConfiguration
 import com.rarible.protocol.union.enrichment.repository.search.EsActivityRepository
 import org.springframework.boot.context.properties.EnableConfigurationProperties
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.Import
     value = [
         SearchConfiguration::class,
         CoreConfiguration::class,
+        EnrichmentApiConfiguration::class,
     ]
 )
 class UnionIndexerConfiguration
