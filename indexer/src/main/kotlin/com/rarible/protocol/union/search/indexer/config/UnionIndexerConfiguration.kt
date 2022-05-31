573f3366e4e83a1917b92ea10d8333ce8b00b0cf
Denis Kilchichakov <kdenis87@gmail.com>
2022-05-31 16:12:49 +0300

    ALPHA-558: config to indexer (final х5)

--- indexer/src/main/kotlin/com/rarible/protocol/union/search/indexer/config/UnionIndexerConfiguration.kt
+++ indexer/src/main/kotlin/com/rarible/protocol/union/search/indexer/config/UnionIndexerConfiguration.kt
@@ -1,8 +1,6 @@
 package com.rarible.protocol.union.search.indexer.config
 
 import com.rarible.protocol.union.core.CoreConfiguration
-import com.rarible.protocol.union.enrichment.configuration.EnrichmentApiConfiguration
-import com.rarible.protocol.union.enrichment.configuration.EnrichmentConfiguration
 import com.rarible.protocol.union.enrichment.configuration.SearchConfiguration
 import com.rarible.protocol.union.enrichment.repository.search.EsActivityRepository
 import com.rarible.protocol.union.integration.ethereum.EthereumApiConfiguration
@@ -10,7 +8,6 @@ import com.rarible.protocol.union.integration.ethereum.PolygonApiConfiguration
 import com.rarible.protocol.union.integration.flow.FlowApiConfiguration
 import com.rarible.protocol.union.integration.immutablex.ImmutablexApiConfiguration
 import com.rarible.protocol.union.integration.solana.SolanaApiConfiguration
-import com.rarible.protocol.union.integration.tezos.TezosApiConfiguration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
