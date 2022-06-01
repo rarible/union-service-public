0495573b9d2863623b6ce76669fa54cc2b9b73c9
Sergey Ratushnyak <39464298+Rtserg@users.noreply.github.com>
2022-06-01 17:07:33 +0300

    feat: ALPHA-580 integrate Ethereum filters to Union (#349)

--- integration/tezos/src/main/kotlin/com/rarible/protocol/union/integration/tezos/service/TezosActivityService.kt
+++ integration/tezos/src/main/kotlin/com/rarible/protocol/union/integration/tezos/service/TezosActivityService.kt
@@ -25,6 +25,7 @@ import com.rarible.protocol.union.dto.ActivitySortDto
 import com.rarible.protocol.union.dto.ActivityTypeDto
 import com.rarible.protocol.union.dto.BlockchainDto
 import com.rarible.protocol.union.dto.SyncSortDto
+import com.rarible.protocol.union.dto.SyncTypeDto
 import com.rarible.protocol.union.dto.UserActivityTypeDto
 import com.rarible.protocol.union.dto.continuation.ActivityContinuation
 import com.rarible.protocol.union.dto.continuation.page.Paging
@@ -70,7 +71,8 @@ open class TezosActivityService(
     override suspend fun getAllActivitiesSync(
         continuation: String?,
         size: Int,
-        sort: SyncSortDto?
+        sort: SyncSortDto?,
+        type: SyncTypeDto?
     ): Slice<ActivityDto> = Slice.empty()
 
     override suspend fun getActivitiesByCollection(
