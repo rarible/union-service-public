ec638df01c1ddd4fe6fcea31091e6e3abe80898c
Denis Kilchichakov <kdenis87@gmail.com>
2022-06-06 17:49:56 +0300

    ALPHA-602: sort collections by date+salt

--- core/src/main/kotlin/com/rarible/protocol/union/core/model/EsCollection.kt
+++ core/src/main/kotlin/com/rarible/protocol/union/core/model/EsCollection.kt
@@ -4,21 +4,34 @@ import com.rarible.protocol.union.core.model.elasticsearch.EsEntity
 import com.rarible.protocol.union.core.model.elasticsearch.EntityDefinition
 import com.rarible.protocol.union.core.model.elasticsearch.EsEntitiesConfig.INDEX_SETTINGS
 import com.rarible.protocol.union.core.model.elasticsearch.EsEntitiesConfig.loadMapping
+import com.rarible.protocol.union.core.model.elasticsearch.generateSalt
 import com.rarible.protocol.union.dto.BlockchainDto
 import org.springframework.data.annotation.Id
+import org.springframework.data.elasticsearch.annotations.Field
+import org.springframework.data.elasticsearch.annotations.FieldType
+import java.time.Instant
 
 sealed class EsCollectionSealed {
     abstract val collectionId: String // blockchain:value
+    abstract val date: Instant
+    abstract val salt: Long
 }
 
 data class EsCollectionLite(
     override val collectionId: String, //blockchain:value
+    override val date: Instant,
+    override val salt: Long,
 ) : EsCollectionSealed()
 
 data class EsCollection(
     @Id
     override val collectionId: String, //blockchain:value
 
+    // Sort fields
+    @Field(type = FieldType.Date)
+    override val date: Instant,
+    override val salt: Long = generateSalt(),
+    // Filter fields
     val blockchain: BlockchainDto,
     val name: String,
     val symbol: String? = null,
---
+++ enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/repository/search/internal/EsQueryCursorUtils.kt
@@ -0,0 +1,45 @@
+package com.rarible.protocol.union.enrichment.repository.search.internal
+
+import org.elasticsearch.index.query.BoolQueryBuilder
+import org.elasticsearch.index.query.QueryBuilder
+import org.elasticsearch.index.query.RangeQueryBuilder
+import org.elasticsearch.index.query.TermQueryBuilder
+import java.time.Instant
+
+fun BoolQueryBuilder.shouldAll(vararg queryBuilders: () -> QueryBuilder?) {
+    val mustAll = BoolQueryBuilder()
+    queryBuilders.forEach {
+        val query = it()
+        if (query != null) {
+            mustAll.must(query)
+        }
+    }
+    this.should(mustAll)
+}
+
+fun mustEqual(fieldName: String, value: Long): QueryBuilder? {
+    if (value == 0L) return null
+    return TermQueryBuilder(fieldName, value)
+}
+
+fun mustEqual(fieldName: String, value: Int): QueryBuilder? {
+    if (value == 0) return null
+    return TermQueryBuilder(fieldName, value)
+}
+
+fun mustEqual(fieldName: String, value: Instant?): QueryBuilder? {
+    if (value == null) return null
+    return TermQueryBuilder(fieldName, value)
+}
+
+fun mustDiffer(fieldName: String, value: Any?, descending: Boolean): QueryBuilder? {
+    return if (value != null) {
+        val rangeQuery = RangeQueryBuilder(fieldName)
+        if (descending) {
+            rangeQuery.lt(value)
+        } else {
+            rangeQuery.gt(value)
+        }
+        rangeQuery
+    } else null
+}
