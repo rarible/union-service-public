e1cedce0e678f041cfd58801377cc1cd26accf26
Artem Yakischik <a.yakischik@rarible.com>
2022-06-09 18:14:58 +0400

    feat: Embedded content migration

--- core/src/main/kotlin/com/rarible/protocol/union/core/FeatureFlagsProperties.kt
+++ core/src/main/kotlin/com/rarible/protocol/union/core/FeatureFlagsProperties.kt
@@ -12,6 +12,7 @@ data class FeatureFlagsProperties(
     val enableItemLastSaleEnrichment: Boolean = true,
     val enableLegacyWrappedEventTopic: Boolean = true,
     val enableContentMetaCache: Boolean = true,
+    val enableEmbeddedContentMigrationJob: Boolean = true,
     // activities
     var enableActivityQueriesToElasticSearch: Boolean = false,
     var enableActivityQueriesPerTypeFilter: Boolean = false,
