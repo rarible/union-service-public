823de9f4e41d742b2ad73605ee14ecc148ac2661
Denis Kilchichakov <kdenis87@gmail.com>
2022-06-21 13:20:52 +0300

    PT-560: added cursor to every activity returned by ES

--- api/src/main/kotlin/com/rarible/protocol/union/api/service/elastic/ActivityElasticService.kt
+++ api/src/main/kotlin/com/rarible/protocol/union/api/service/elastic/ActivityElasticService.kt
@@ -4,6 +4,7 @@ import com.rarible.core.apm.CaptureSpan
 import com.rarible.core.apm.SpanType
 import com.rarible.core.common.mapAsync
 import com.rarible.core.logging.Logger
+import com.rarible.protocol.union.core.model.EsActivityCursor.Companion.fromActivityLite
 import com.rarible.protocol.union.enrichment.service.query.activity.ActivityQueryService
 import com.rarible.protocol.union.core.model.TypedActivityId
 import com.rarible.protocol.union.core.service.ActivityService
@@ -16,8 +17,25 @@ import com.rarible.protocol.union.dto.BlockchainDto
 import com.rarible.protocol.union.dto.UserActivityTypeDto
 import com.rarible.protocol.union.core.model.EsActivityLite
 import com.rarible.protocol.union.core.model.EsActivitySort
+import com.rarible.protocol.union.dto.AuctionBidActivityDto
+import com.rarible.protocol.union.dto.AuctionCancelActivityDto
+import com.rarible.protocol.union.dto.AuctionEndActivityDto
+import com.rarible.protocol.union.dto.AuctionFinishActivityDto
+import com.rarible.protocol.union.dto.AuctionOpenActivityDto
+import com.rarible.protocol.union.dto.AuctionStartActivityDto
+import com.rarible.protocol.union.dto.BurnActivityDto
+import com.rarible.protocol.union.dto.L2DepositActivityDto
+import com.rarible.protocol.union.dto.L2WithdrawalActivityDto
+import com.rarible.protocol.union.dto.MintActivityDto
+import com.rarible.protocol.union.dto.OrderBidActivityDto
+import com.rarible.protocol.union.dto.OrderCancelBidActivityDto
+import com.rarible.protocol.union.dto.OrderCancelListActivityDto
+import com.rarible.protocol.union.dto.OrderListActivityDto
+import com.rarible.protocol.union.dto.OrderMatchSellDto
+import com.rarible.protocol.union.dto.OrderMatchSwapDto
 import com.rarible.protocol.union.dto.SyncSortDto
 import com.rarible.protocol.union.dto.SyncTypeDto
+import com.rarible.protocol.union.dto.TransferActivityDto
 import com.rarible.protocol.union.dto.parser.IdParser
 import com.rarible.protocol.union.enrichment.repository.search.EsActivityRepository
 import org.springframework.stereotype.Service
@@ -146,7 +164,7 @@ class ActivityElasticService(
 
         val activitiesIdMapping = activities.associateBy { it.id.fullId() }
         return esActivities.mapNotNull { esActivity ->
-            activitiesIdMapping[esActivity.activityId]
+            applyCursor(activitiesIdMapping[esActivity.activityId], esActivity.fromActivityLite().toString())
         }
     }
 
@@ -156,5 +174,28 @@ class ActivityElasticService(
             ActivitySortDto.EARLIEST_FIRST -> false
         }
         return EsActivitySort(latestFirst)
+    }
+
+    private fun applyCursor(activityDto: ActivityDto?, cursor: String): ActivityDto? {
+        if (activityDto == null) return null
+        return when (activityDto) {
+            is MintActivityDto -> activityDto.copy(cursor = cursor)
+            is BurnActivityDto -> activityDto.copy(cursor = cursor)
+            is TransferActivityDto -> activityDto.copy(cursor = cursor)
+            is OrderMatchSwapDto -> activityDto.copy(cursor = cursor)
+            is OrderMatchSellDto -> activityDto.copy(cursor = cursor)
+            is OrderBidActivityDto -> activityDto.copy(cursor = cursor)
+            is OrderListActivityDto -> activityDto.copy(cursor = cursor)
+            is OrderCancelBidActivityDto -> activityDto.copy(cursor = cursor)
+            is OrderCancelListActivityDto -> activityDto.copy(cursor = cursor)
+            is AuctionOpenActivityDto -> activityDto.copy(cursor = cursor)
+            is AuctionBidActivityDto -> activityDto.copy(cursor = cursor)
+            is AuctionFinishActivityDto -> activityDto.copy(cursor = cursor)
+            is AuctionCancelActivityDto -> activityDto.copy(cursor = cursor)
+            is AuctionStartActivityDto -> activityDto.copy(cursor = cursor)
+            is AuctionEndActivityDto -> activityDto.copy(cursor = cursor)
+            is L2DepositActivityDto -> activityDto.copy(cursor = cursor)
+            is L2WithdrawalActivityDto -> activityDto.copy(cursor = cursor)
+        }
     }
 }
