665e307a80b9c55fba606c89023c7a023d652e7a
Denis Kilchichakov <augur@users.noreply.github.com>
2022-06-30 14:22:43 +0300

    Merge 1.28 to 1.29 (#413)
    * Disabled Items indexing/reindexing
    
    * fix: Fix indexer config
    
    Co-authored-by: sanufriev <s.anufriev@rarible.com>

--- indexer/src/main/kotlin/com/rarible/protocol/union/search/indexer/config/KafkaProperties.kt
+++ indexer/src/main/kotlin/com/rarible/protocol/union/search/indexer/config/KafkaProperties.kt
@@ -5,9 +5,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConstructorBinding
-@ConfigurationProperties(prefix = "listener.consumer")
+@ConfigurationProperties(prefix = "indexer.consumer")
 class KafkaProperties(
     val brokerReplicaSet: String,
-    val workerCount: Int = 10,
+    val workerCount: Int = 3,
     val daemon: DaemonWorkerProperties = DaemonWorkerProperties(),
 )
