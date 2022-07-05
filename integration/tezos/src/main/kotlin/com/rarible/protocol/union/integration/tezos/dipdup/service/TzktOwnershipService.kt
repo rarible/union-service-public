1c8e6b74e855f008d6310de22ef30648dcf71ae6
Roman Novikov <mystdeim@gmail.com>
2022-07-05 11:09:05 +0300

    fix ownerships all for new indexer

--- integration/tezos/src/main/kotlin/com/rarible/protocol/union/integration/tezos/dipdup/service/TzktOwnershipService.kt
+++ integration/tezos/src/main/kotlin/com/rarible/protocol/union/integration/tezos/dipdup/service/TzktOwnershipService.kt
@@ -1,12 +1,16 @@
 package com.rarible.protocol.union.integration.tezos.dipdup.service
 
-import com.rarible.protocol.union.core.exception.UnionNotFoundException
 import com.rarible.protocol.union.core.model.UnionOwnership
 import com.rarible.protocol.union.dto.continuation.page.Page
+import com.rarible.protocol.union.dto.continuation.page.Slice
 
 interface TzktOwnershipService {
 
     fun enabled() = false
+
+    suspend fun getOwnershipsAll(continuation: String?, size: Int): Slice<UnionOwnership> {
+        TODO("Not implemented")
+    }
 
     suspend fun getOwnershipById(ownershipId: String): UnionOwnership {
         TODO("Not implemented")
