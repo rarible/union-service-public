4368291bd7f9ab2af7cf9b794a9f0ac691c8dc6e
Denis Kilchichakov <kdenis87@gmail.com>
2022-08-03 12:03:22 +0300

    Try fix test on jenkins

--- api/src/test/kotlin/com/rarible/protocol/union/api/controller/ContentControllerFt.kt
+++ api/src/test/kotlin/com/rarible/protocol/union/api/controller/ContentControllerFt.kt
@@ -5,6 +5,7 @@ import com.rarible.protocol.union.api.controller.test.AbstractIntegrationTest
 import com.rarible.protocol.union.api.controller.test.IntegrationTest
 import com.rarible.protocol.union.enrichment.meta.embedded.EmbeddedContentStorage
 import com.rarible.protocol.union.enrichment.meta.embedded.UnionEmbeddedContent
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -27,6 +28,10 @@ class ContentControllerFt : AbstractIntegrationTest() {
             size = data.size,
             data = data
         )
+
+        // Check if test depends on time of server startup
+        delay(15000)
+
         embeddedContentStorage.save(content)
 
         val result = testRestTemplate.getForEntity("$baseUri/content/embedded/${content.id}", ByteArray::class.java)
