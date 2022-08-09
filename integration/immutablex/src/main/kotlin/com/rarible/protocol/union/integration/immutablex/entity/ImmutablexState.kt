e59fff76cb3039b729d487f6cbad2a1447b9fdee
Artem Yakischik <a.yakischik@rarible.com>
2022-08-09 11:41:37 +0400

    Minor IMX scanner refactoring

--- integration/immutablex/src/main/kotlin/com/rarible/protocol/union/integration/immutablex/entity/ImmutablexState.kt
+++ integration/immutablex/src/main/kotlin/com/rarible/protocol/union/integration/immutablex/entity/ImmutablexState.kt
@@ -1,18 +1,20 @@
 package com.rarible.protocol.union.integration.immutablex.entity
 
-import java.time.Instant
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.FieldType
 import org.springframework.data.mongodb.core.mapping.MongoId
+import java.time.Instant
 
 @Document("immutablex_state")
 data class ImmutablexState(
     @MongoId(FieldType.STRING)
     val id: String,
     val cursor: String? = null,
+    val entityDate: Instant? = null,
     @LastModifiedDate
     val lastDate: Instant? = null,
     val lastError: String? = null,
+    val lastErrorDate: Instant? = null,
     val lastErrorStacktrace: String? = null
 )
