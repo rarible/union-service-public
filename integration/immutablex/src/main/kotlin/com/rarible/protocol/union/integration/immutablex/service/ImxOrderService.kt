8b8c3e6c5771c63b21466ab2e073692155bbf756
Artem Yakischik <83639804+yakischik@users.noreply.github.com>
2022-08-15 16:56:53 +0400

    feat: Immutablex getBidCurrencies/getSellCurrencies support (#485)

--- integration/immutablex/src/main/kotlin/com/rarible/protocol/union/integration/immutablex/service/ImxOrderService.kt
+++ integration/immutablex/src/main/kotlin/com/rarible/protocol/union/integration/immutablex/service/ImxOrderService.kt
@@ -4,7 +4,6 @@ import com.rarible.protocol.union.core.service.OrderService
 import com.rarible.protocol.union.core.service.router.AbstractBlockchainService
 import com.rarible.protocol.union.dto.AssetTypeDto
 import com.rarible.protocol.union.dto.BlockchainDto
-import com.rarible.protocol.union.dto.EthEthereumAssetTypeDto
 import com.rarible.protocol.union.dto.OrderDto
 import com.rarible.protocol.union.dto.OrderSortDto
 import com.rarible.protocol.union.dto.OrderStatusDto
@@ -17,12 +16,15 @@ import com.rarible.protocol.union.dto.parser.IdParser
 import com.rarible.protocol.union.integration.immutablex.client.ImxOrderClient
 import com.rarible.protocol.union.integration.immutablex.client.TokenIdDecoder
 import com.rarible.protocol.union.integration.immutablex.converter.ImxOrderConverter
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 
 class ImxOrderService(
     private val orderClient: ImxOrderClient
 ) : AbstractBlockchainService(BlockchainDto.IMMUTABLEX), OrderService {
 
-    private val defaultCurrencies = listOf(EthEthereumAssetTypeDto(blockchain))
+    // TODO move out to configuration
+    private val currencyProbeBatchSize = 64
 
     override suspend fun getOrdersAll(
         continuation: String?,
@@ -69,9 +71,12 @@ class ImxOrderService(
         }
     }
 
-    // TODO IMMUTABLEX we need to support ERC20 here?
     override suspend fun getBidCurrencies(itemId: String): List<AssetTypeDto> {
-        return defaultCurrencies
+        // TODO we can miss some orders here if item have a lot of cancelled/filled orders with different currencies
+        // TODO maybe split it into 2 queries to distinguish ETH/ERC20 currencies?
+        val (token, tokenId) = IdParser.split(TokenIdDecoder.decodeItemId(itemId), 2)
+        val orders = orderClient.getBuyOrdersByItem(token, tokenId, null, currencyProbeBatchSize)
+        return orders.result.map { ImxOrderConverter.convert(it, blockchain).make.type }.toSet().toList()
     }
 
     // IMX doesn't support floor orders
@@ -79,7 +84,6 @@ class ImxOrderService(
         return emptyList()
     }
 
-    // TODO IMMUTABLEX we can support it for non-active statuses
     override suspend fun getOrderBidsByItem(
         platform: PlatformDto?,
         itemId: String,
@@ -116,9 +120,24 @@ class ImxOrderService(
         return Paging(OrderContinuation.ByLastUpdatedAndIdDesc, orders).getSlice(size)
     }
 
-    // TODO IMMUTABLEX we need to support ERC20 here?
     override suspend fun getSellCurrencies(itemId: String): List<AssetTypeDto> {
-        return defaultCurrencies
+        // TODO we can miss some orders here if item have a lot of cancelled/filled orders with different currencies
+        // TODO maybe split it into 2 queries to distinguish ETH/ERC20 currencies?
+        val (token, tokenId) = IdParser.split(TokenIdDecoder.decodeItemId(itemId), 2)
+        // We MUST request active order in order to definitely have it as 'best sell'
+        val activeOrder = coroutineScope {
+            async {
+                orderClient.getSellOrdersByItem(
+                    token, tokenId, OrderStatusDto.ACTIVE, null, currencyProbeBatchSize
+                )
+            }
+        }
+
+        val ordersWithAllStatuses = orderClient.getSellOrdersByItem(
+            token, tokenId, null, null, currencyProbeBatchSize
+        ).result + activeOrder.await().result
+
+        return ordersWithAllStatuses.map { ImxOrderConverter.convert(it, blockchain).take.type }.toSet().toList()
     }
 
     // IMX doesn't support floor orders
