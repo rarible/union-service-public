9f0a1738937806c2d8395c60e394807796c4a609
Roman Novikov <mystdeim@gmail.com>
2022-09-29 17:29:45 +0100

    wrap dipdup notfound exception

--- integration/tezos/src/main/kotlin/com/rarible/protocol/union/integration/tezos/dipdup/service/DipDupRoyaltyService.kt
+++ integration/tezos/src/main/kotlin/com/rarible/protocol/union/integration/tezos/dipdup/service/DipDupRoyaltyService.kt
@@ -1,15 +1,24 @@
 package com.rarible.protocol.union.integration.tezos.dipdup.service
 
 import com.rarible.dipdup.client.RoyaltiesClient
+import com.rarible.dipdup.client.exception.DipDupNotFound
 import com.rarible.protocol.union.dto.RoyaltyDto
 import com.rarible.protocol.union.integration.tezos.dipdup.converter.DipDupItemConverter
+import org.slf4j.LoggerFactory
 
 class DipDupRoyaltyService(
     private val dipDupRoyaltyClient: RoyaltiesClient
 ) {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     suspend fun getItemRoyaltiesById(itemId: String): List<RoyaltyDto> {
-        val royalties = dipDupRoyaltyClient.getRoyaltiesById(itemId)
-        return DipDupItemConverter.convert(royalties)
+        return try {
+            val royalties = dipDupRoyaltyClient.getRoyaltiesById(itemId)
+            DipDupItemConverter.convert(royalties)
+        } catch (ex: DipDupNotFound) {
+            logger.debug("Royalty wasn't found")
+            emptyList()
+        }
     }
 }
