807f29f35e19756c3fd8702550980e8345805dfc
Roman Novikov <mystdeim@gmail.com>
2022-10-06 20:09:45 +0100

    fix test

--- integration/tezos/src/test/kotlin/com/rarible/protocol/union/integration/tezos/data/TezosDataFactory.kt
+++ integration/tezos/src/test/kotlin/com/rarible/protocol/union/integration/tezos/data/TezosDataFactory.kt
@@ -102,7 +102,8 @@ fun randomTezosDipDupItemDto(itemId: ItemIdDto): DipDupItem {
         updated = Instant.now(),
         contract = contract,
         deleted = false,
-        tzktId = BigInteger.ONE
+        tzktId = BigInteger.ONE,
+        creator = randomString()
     )
 }
 
