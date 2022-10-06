3fefc8e68f4f80ffed116f2cbafa6b6ffae3c79d
Denis Kilchichakov <kdenis87@gmail.com>
2022-10-06 12:17:50 +0300

    PT-1343: added endpoint to schedule ownership reindex

--- api/src/main/kotlin/com/rarible/protocol/union/api/controller/internal/EsMaintenanceController.kt
+++ api/src/main/kotlin/com/rarible/protocol/union/api/controller/internal/EsMaintenanceController.kt
@@ -1,6 +1,7 @@
 package com.rarible.protocol.union.api.controller.internal
 
 import com.rarible.protocol.union.api.model.internal.EsMaintenanceReindexActivityDto
+import com.rarible.protocol.union.api.model.internal.EsMaintenanceReindexOwnershipDto
 import com.rarible.protocol.union.api.service.elastic.ElasticMaintenanceService
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
@@ -29,5 +30,20 @@ class EsMaintenanceController(
             esIndex = payload.esIndex,
         )
         logger.info("Reindex activities tasks scheduled: $payload")
+    }
+
+    @PostMapping(
+        value = ["/maintenance/es/reindex/ownership"],
+        consumes = ["application/json"]
+    )
+    suspend fun reindexOwnerships(@RequestBody payload: EsMaintenanceReindexOwnershipDto) {
+        logger.info("Got request to reindex ownerships: $payload")
+        elasticMaintenanceService.scheduleReindexOwnershipsTasks(
+            blockchains = payload.blockchains,
+            from = payload.from,
+            to = payload.to,
+            esIndex = payload.esIndex,
+        )
+        logger.info("Reindex ownerships tasks scheduled: $payload")
     }
 }
