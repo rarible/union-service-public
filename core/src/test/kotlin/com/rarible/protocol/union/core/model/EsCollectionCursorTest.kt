e563c907e3c2b143cf1ae154f8f31fc9b30d812f
Sergey Yurov <s.yurov@rarible.com>
2022-10-09 13:21:53 +0300

    fix "null"
    (cherry picked from commit 9053c17381a922579ddb32751f48a5cba3e3dbd1)

---
+++ core/src/test/kotlin/com/rarible/protocol/union/core/model/EsCollectionCursorTest.kt
@@ -0,0 +1,30 @@
+package com.rarible.protocol.union.core.model
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.TestFactory
+import java.time.Instant
+
+internal class EsCollectionCursorTest {
+
+    @TestFactory
+    fun `should convert from`() = listOf(
+        TestCase("", null),
+        TestCase("_1000", null),
+        TestCase("null_1000", null),
+        TestCase("1665310765200_1000", EsCollectionCursor(Instant.ofEpochMilli(1665310765200), 1000)),
+    ).map {
+        dynamicTest("should convert from $it") {
+            val continuation = EsCollectionCursor.fromString(it.str)
+
+            if (it.result == null) {
+                assertNull(continuation)
+            } else {
+                assertEquals(it.result, continuation)
+            }
+        }
+    }
+}
+
+data class TestCase(val str: String, val result: EsCollectionCursor?)
