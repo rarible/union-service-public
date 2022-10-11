b74c596fbea6cd44800df4e4abcc4e3fcbfa6d70
Denis Kilchichakov <kdenis87@gmail.com>
2022-10-11 13:20:09 +0300

    fix: old cursor for es items request

--- enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/repository/search/internal/EsEntitySearchAfterCursorService.kt
+++ enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/repository/search/internal/EsEntitySearchAfterCursorService.kt
@@ -1,10 +1,23 @@
 package com.rarible.protocol.union.enrichment.repository.search.internal
 
+import org.slf4j.LoggerFactory
 import org.springframework.data.elasticsearch.core.SearchHit
 import org.springframework.stereotype.Component
 
 @Component
 class EsEntitySearchAfterCursorService {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    fun tryFixLegacyCursor(cursor: String?): String? {
+        if (cursor.isNullOrBlank()) return cursor
+        return if (cursor.first().isLetter()) {
+            logger.info("Attempt to fix legacy cursor $cursor")
+            cursor.substringAfter(":")
+        } else {
+            cursor
+        }
+    }
 
     fun buildSearchAfterClause(cursorAsString: String?): List<Any>? {
         if (cursorAsString.isNullOrEmpty()) return null
