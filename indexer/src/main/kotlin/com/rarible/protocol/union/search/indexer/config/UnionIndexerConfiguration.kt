8534011805038dc5c69596a6c0706b6d3bc81801
Roman Novikov <mystdeim@gmail.com>
2022-11-09 07:46:35 +0000

    enable tezos in the indexer

--- indexer/src/main/kotlin/com/rarible/protocol/union/search/indexer/config/UnionIndexerConfiguration.kt
+++ indexer/src/main/kotlin/com/rarible/protocol/union/search/indexer/config/UnionIndexerConfiguration.kt
@@ -9,6 +9,7 @@ import com.rarible.protocol.union.integration.ethereum.PolygonApiConfiguration
 import com.rarible.protocol.union.integration.flow.FlowApiConfiguration
 import com.rarible.protocol.union.integration.immutablex.ImxApiConfiguration
 import com.rarible.protocol.union.integration.solana.SolanaApiConfiguration
+import com.rarible.protocol.union.integration.tezos.TezosApiConfiguration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
@@ -23,7 +24,7 @@ import org.springframework.context.annotation.Import
         EthereumApiConfiguration::class,
         PolygonApiConfiguration::class,
         FlowApiConfiguration::class,
-        // TezosApiConfiguration::class, // TODO enable with proper config
+        TezosApiConfiguration::class, // TODO enable with proper config
         SolanaApiConfiguration::class,
         ImxApiConfiguration::class,
         SearchConfiguration::class,
