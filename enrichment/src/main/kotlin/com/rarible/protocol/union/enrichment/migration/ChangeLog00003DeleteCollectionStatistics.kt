1ae824f6f36de205639b3e9edcd755aa076972d3
Roman Novikov <mystdeim@gmail.com>
2023-03-02 13:27:33 +0000

    fix: remove statistics field

---
+++ enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/migration/ChangeLog00003DeleteCollectionStatistics.kt
@@ -0,0 +1,32 @@
+package com.rarible.protocol.union.enrichment.migration
+
+import com.github.cloudyrock.mongock.ChangeLog
+import com.github.cloudyrock.mongock.ChangeSet
+import com.rarible.protocol.union.enrichment.model.ShortCollection
+import io.changock.migration.api.annotations.NonLockGuarded
+import kotlinx.coroutines.reactive.awaitFirstOrNull
+import kotlinx.coroutines.runBlocking
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.Update
+
+@ChangeLog(order = "2")
+class ChangeLog00003DeleteCollectionStatistics {
+
+    @ChangeSet(
+        id = "ChangeLog00003DeleteCollectionStatistics.deleteCollectionStatistics",
+        order = "1",
+        author = "protocol"
+    )
+    fun deleteCollectionStatistics(
+        @NonLockGuarded template: ReactiveMongoTemplate
+    ) = runBlocking {
+        val query = Query(Criteria.where(STATISTICS).exists(true))
+        template.updateMulti(query, Update().unset(STATISTICS), ShortCollection::class.java).awaitFirstOrNull()
+    }
+
+    companion object {
+        const val STATISTICS = "statistics"
+    }
+}
---
+++ enrichment/src/test/kotlin/com/rarible/protocol/union/enrichment/migration/DeleteCollectionStatistics.kt
@@ -0,0 +1,34 @@
+package com.rarible.protocol.union.enrichment.migration
+
+import com.mongodb.reactivestreams.client.MongoClients
+import com.rarible.core.test.containers.MongodbTestContainer
+import com.rarible.core.test.data.randomString
+import com.rarible.protocol.union.enrichment.migration.ChangeLog00003DeleteCollectionStatistics.Companion.STATISTICS
+import com.rarible.protocol.union.enrichment.model.ShortCollection
+import kotlinx.coroutines.reactive.awaitFirstOrNull
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+
+class DeleteCollectionStatistics {
+
+    private val mongoTest = MongodbTestContainer()
+    private val mongoTemplate = ReactiveMongoTemplate(MongoClients.create(mongoTest.connectionString()), "test")
+
+    @Test
+    fun `should remove statistics from collections`() = runBlocking<Unit> {
+        mongoTemplate.insert(
+            mapOf("collectionId" to randomString(),
+            STATISTICS to "b"), "enrichment_collection").awaitFirstOrNull()
+
+        val migration = ChangeLog00003DeleteCollectionStatistics()
+        migration.deleteCollectionStatistics(mongoTemplate)
+
+        val collection = mongoTemplate.find(Query(Criteria.where(STATISTICS).exists(true)), ShortCollection::class.java).awaitFirstOrNull()
+        assertThat(collection).isNull()
+    }
+
+}
