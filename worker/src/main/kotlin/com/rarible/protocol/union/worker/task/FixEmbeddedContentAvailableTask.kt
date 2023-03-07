70225a69d1a4a980e416be41134b40449c734fbe
Dmitry Saraykin <d.saraykin@rarible.com>
2023-03-07 17:22:36 +0300

    fix(MPX-1572): fix embedded content availability. cr comments

--- worker/src/main/kotlin/com/rarible/protocol/union/worker/task/FixEmbeddedContentAvailableTask.kt
+++ worker/src/main/kotlin/com/rarible/protocol/union/worker/task/FixEmbeddedContentAvailableTask.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.collectIndexed
 import kotlinx.coroutines.flow.flow
 import org.springframework.stereotype.Component
 
+@Deprecated("remove after release")
 @Component
 class FixEmbeddedContentAvailableTask(
     private val itemRepository: ItemRepository,
