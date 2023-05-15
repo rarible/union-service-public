78af6ed419fe400e46e4f715119eb3028f3f10da
Artem Yakischik <a.yakischik@rarible.com>
2023-05-15 12:11:34 +0400

    Mistype fix

--- enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/model/ItemMetaCustomAttributes.kt
+++ enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/model/ItemMetaCustomAttributes.kt
@@ -2,7 +2,9 @@ package com.rarible.protocol.union.enrichment.model
 
 import com.rarible.protocol.union.core.model.UnionMetaAttribute
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 
+@Document("meta-custom-attributes")
 data class ItemMetaCustomAttributes(
     @Id
     val id: String,
