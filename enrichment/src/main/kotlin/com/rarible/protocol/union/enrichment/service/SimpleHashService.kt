9fd3e73e971033d02360dc5d1a6632f97a3cebf8
Roman Novikov <mystdeim@gmail.com>
2023-05-24 13:38:58 +0100

    feat: skipped getting meta for lazy items

--- enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/service/SimpleHashService.kt
+++ enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/service/SimpleHashService.kt
@@ -1,6 +1,8 @@
 package com.rarible.protocol.union.enrichment.service
 
 import com.rarible.protocol.union.core.model.UnionMeta
+import com.rarible.protocol.union.core.service.ItemService
+import com.rarible.protocol.union.core.service.router.BlockchainRouter
 import com.rarible.protocol.union.dto.BlockchainDto
 import com.rarible.protocol.union.dto.CollectionIdDto
 import com.rarible.protocol.union.dto.ItemIdDto
@@ -14,6 +16,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import java.math.BigInteger
 
 @Service
 @ConditionalOnProperty("meta.simpleHash.enabled", havingValue = "true")
@@ -21,6 +24,7 @@ class SimpleHashService(
     private val props: UnionMetaProperties,
     private val simpleHashClient: WebClient,
     private val metrics: ItemMetaMetrics,
+    private val itemServiceRouter: BlockchainRouter<ItemService>,
 ) {
 
     private val logger = LoggerFactory.getLogger(javaClass)
@@ -31,11 +35,15 @@ class SimpleHashService(
 
     suspend fun fetch(key: ItemIdDto): UnionMeta? {
         try {
-            val response = simpleHashClient.get()
-                .uri("/nfts/${network(key.blockchain)}/${key.value.replace(":", "/")}")
-                .retrieve().bodyToMono(SimpleHashItem::class.java).awaitSingle()
-            metrics.onMetaFetched(key.blockchain, MetaSource.SIMPLE_HASH)
-            return SimpleHashConverter.convert(response)
+            if (isLazy(key)) {
+                logger.info("Skipped to fetch from simplehash $key because it's lazy item")
+            } else {
+                val response = simpleHashClient.get()
+                    .uri("/nfts/${network(key.blockchain)}/${key.value.replace(":", "/")}")
+                    .retrieve().bodyToMono(SimpleHashItem::class.java).awaitSingle()
+                metrics.onMetaFetched(key.blockchain, MetaSource.SIMPLE_HASH)
+                return SimpleHashConverter.convert(response)
+            }
         } catch (e: Exception) {
             metrics.onMetaError(key.blockchain, MetaSource.SIMPLE_HASH)
             logger.error("Failed to fetch from simplehash $key", e)
@@ -50,6 +58,11 @@ class SimpleHashService(
 
     private fun network(blockchain: BlockchainDto): String {
         return props.simpleHash.mapping[blockchain.name.lowercase()] ?: blockchain.name.lowercase()
+    }
+
+    private suspend fun isLazy(key: ItemIdDto): Boolean {
+        val item = itemServiceRouter.getService(key.blockchain).getItemById(key.value)
+        return item?.lazySupply > BigInteger.ZERO
     }
 
 }
