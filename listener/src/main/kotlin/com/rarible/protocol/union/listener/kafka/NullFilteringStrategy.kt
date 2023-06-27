9dcab3875abd7546e840efaf8240aa7a1776033c
Artem Yakischik <a.yakischik@rarible.com>
2023-06-27 12:15:33 +0400

    Common kafka classes removed

--- listener/src/main/kotlin/com/rarible/protocol/union/listener/kafka/NullFilteringStrategy.kt
+++
@@ -1,8 +0,0 @@
-package com.rarible.protocol.union.listener.kafka
-
-import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.springframework.kafka.listener.adapter.RecordFilterStrategy
-
-class NullFilteringStrategy<V> : RecordFilterStrategy<String, V> {
-    override fun filter(consumerRecord: ConsumerRecord<String, V?>): Boolean = consumerRecord.value() == null
-}
