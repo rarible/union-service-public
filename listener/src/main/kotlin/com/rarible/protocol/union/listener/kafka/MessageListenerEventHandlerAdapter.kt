67de6175afaeb4fb65b2eab225808dea282a15ed
Artem Yakischik <a.yakischik@rarible.com>
2023-07-04 18:20:45 +0400

    Debug logs removed

--- listener/src/main/kotlin/com/rarible/protocol/union/listener/kafka/MessageListenerEventHandlerAdapter.kt
+++ listener/src/main/kotlin/com/rarible/protocol/union/listener/kafka/MessageListenerEventHandlerAdapter.kt
@@ -8,20 +8,15 @@ import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.slf4j.LoggerFactory
 import org.springframework.kafka.listener.BatchMessageListener
-import java.util.UUID
 
 class MessageListenerEventHandlerAdapter<T>(
     private val handler: InternalEventHandler<T>
 ) : BatchMessageListener<String, T> {
 
-    private val listenerId = UUID.randomUUID()
-
     override fun onMessage(records: List<ConsumerRecord<String, T>>) = runBlocking<Unit>(NonCancellable) {
         val now = System.currentTimeMillis()
         withBatchId {
-            logger.info("Starting to handle batch of {} entities by listener {}", records.size, listenerId)
             val recordsByKey = records.groupBy { it.key() }
             recordsByKey.values.map { group ->
                 asyncWithTraceId(context = NonCancellable) {
@@ -32,11 +27,6 @@ class MessageListenerEventHandlerAdapter<T>(
                     }
                 }
             }.awaitAll()
-            logger.info("Finished to handle batch by {} ({}ms)", listenerId, System.currentTimeMillis() - now)
         }
-    }
-
-    companion object {
-        private val logger = LoggerFactory.getLogger(MessageListenerEventHandlerAdapter::class.java)
     }
 }
