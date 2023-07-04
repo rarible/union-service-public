0df6322ceff8a852c3a30bc4adac206891f3ca54
Artem Yakischik <a.yakischik@rarible.com>
2023-07-04 17:43:32 +0400

    More logging

--- core/src/main/kotlin/com/rarible/protocol/union/core/handler/AbstractBlockchainEventHandler.kt
+++ core/src/main/kotlin/com/rarible/protocol/union/core/handler/AbstractBlockchainEventHandler.kt
@@ -4,11 +4,16 @@ import com.rarible.core.apm.withTransaction
 import com.rarible.protocol.union.core.event.EventType
 import com.rarible.protocol.union.core.util.capitalise
 import com.rarible.protocol.union.dto.BlockchainDto
+import org.slf4j.LoggerFactory
 
 abstract class AbstractBlockchainEventHandler<B, U>(
     final override val blockchain: BlockchainDto,
     final override val eventType: EventType
 ) : BlockchainEventHandler<B, U> {
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(AbstractBlockchainEventHandler::class.java)
+    }
 
     // Like ItemEvent#POLYGON
     private val singleEventTransactionName = "${capitalise(eventType.value)}Event#$blockchain"
@@ -24,10 +29,12 @@ abstract class AbstractBlockchainEventHandler<B, U>(
     }
 
     override suspend fun handle(events: List<B>) = withTransaction(singleEventTransactionName) {
+        val start = System.currentTimeMillis()
         val unionEvents = events.mapNotNull { convertWithExceptionHandler(it) }
         if (unionEvents.isNotEmpty()) {
             handler.onEvents(unionEvents)
         }
+        logger.info("Handled {} incoming {} events ({}ms)", events.size, eventType, System.currentTimeMillis() - start)
     }
 
     private suspend fun convertWithExceptionHandler(blockchainEvent: B): U? {
