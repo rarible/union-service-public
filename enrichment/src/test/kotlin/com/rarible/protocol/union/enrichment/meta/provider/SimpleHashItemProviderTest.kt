e1a6e383decbef847c0837e0616fd55b4739162c
Dmitry Saraykin <31763675+dmitry-saraykin@users.noreply.github.com>
2023-07-05 11:38:37 +0300

    feat(PT-3196): retry if no content from simple hash (#730)

---
+++ enrichment/src/test/kotlin/com/rarible/protocol/union/enrichment/meta/provider/SimpleHashItemProviderTest.kt
@@ -0,0 +1,54 @@
+package com.rarible.protocol.union.enrichment.meta.provider
+
+import com.rarible.protocol.union.core.model.download.MetaProviderType
+import com.rarible.protocol.union.core.model.download.ProviderDownloadException
+import com.rarible.protocol.union.dto.BlockchainDto
+import com.rarible.protocol.union.enrichment.service.SimpleHashService
+import com.rarible.protocol.union.enrichment.test.data.randomUnionMeta
+import com.rarible.protocol.union.integration.ethereum.data.randomEthItemId
+import io.mockk.coEvery
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.fail
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+internal class SimpleHashItemProviderTest {
+    @InjectMockKs
+    private lateinit var simpleHashItemProvider: SimpleHashItemProvider
+
+    @MockK
+    private lateinit var simpleHashService: SimpleHashService
+
+    @Test
+    fun `should throw exception if no content`() = runBlocking<Unit> {
+        val itemId = randomEthItemId()
+        coEvery { simpleHashService.isSupported(BlockchainDto.ETHEREUM) } returns true
+        coEvery { simpleHashService.fetch(itemId) } returns randomUnionMeta(content = emptyList())
+
+        try {
+            simpleHashItemProvider.fetch(itemId, null)
+            fail("Shouldn't be here")
+        } catch (e: ProviderDownloadException) {
+            assertThat(e.provider).isEqualTo(MetaProviderType.SIMPLE_HASH)
+        }
+    }
+
+    @Test
+    fun `should throw exception if no meta`() = runBlocking<Unit> {
+        coEvery { simpleHashService.isSupported(BlockchainDto.ETHEREUM) } returns true
+        val itemId = randomEthItemId()
+        coEvery { simpleHashService.fetch(itemId) } returns null
+
+        try {
+            simpleHashItemProvider.fetch(itemId, null)
+            fail("Shouldn't be here")
+        } catch (e: ProviderDownloadException) {
+            assertThat(e.provider).isEqualTo(MetaProviderType.SIMPLE_HASH)
+        }
+    }
+}
