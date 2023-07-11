f6dc6429170cbb590d354abcd960fa6ffaf851d2
Roman Novikov <mystdeim@gmail.com>
2023-07-11 15:17:50 +0100

    fix: set default number of consumers

--- core/src/main/kotlin/com/rarible/protocol/union/core/DefaultConsumerProperties.kt
+++ core/src/main/kotlin/com/rarible/protocol/union/core/DefaultConsumerProperties.kt
@@ -2,7 +2,7 @@ package com.rarible.protocol.union.core
 
 open class DefaultConsumerProperties(
     val brokerReplicaSet: String?,
-    val workers: Map<String, Int>,
+    val workers: Map<String, Int> = mapOf(),
     val batchSize: Int = 32, // TODO ideally, make it configurable per topic
     val username: String? = null,
     val password: String? = null
