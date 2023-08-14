d298a37c262c7a4bf8d3f29b2c00229bda647eb5
Roman Novikov <mystdeim@gmail.com>
2023-08-14 13:36:06 +0100

    fix: adjust sh metrics

--- enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/meta/simplehash/SimpleHashItemMetrics.kt
+++ enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/meta/simplehash/SimpleHashItemMetrics.kt
@@ -20,19 +20,19 @@ class SimpleHashItemMetrics(
         increment(SIMPLEHASH_EVENT_INCOMING, type(TYPE), status("failed"))
     }
 
-    fun onMetaCacheChanged(blockchain: BlockchainDto) {
-        increment(SIMPLEHASH_META_CACHE, type(TYPE), tag(blockchain), status("changed"))
+    fun onMetaItemChanged(blockchain: BlockchainDto) {
+        increment(SIMPLEHASH_META_ITEM, type(TYPE), tag(blockchain), status("changed"))
     }
-    fun onMetaCacheSaved(blockchain: BlockchainDto) {
-        increment(SIMPLEHASH_META_CACHE, type(TYPE), tag(blockchain), status("saved"))
+    fun onMetaItemIgnored(blockchain: BlockchainDto) {
+        increment(SIMPLEHASH_META_ITEM, type(TYPE), tag(blockchain), status("ignored"))
     }
-    fun onMetaCacheRefresh(blockchain: BlockchainDto) {
-        increment(SIMPLEHASH_META_CACHE, type(TYPE), tag(blockchain), status("refresh"))
+    fun onMetaItemRefresh(blockchain: BlockchainDto) {
+        increment(SIMPLEHASH_META_ITEM, type(TYPE), tag(blockchain), status("refresh"))
     }
 
     private companion object {
         const val TYPE: String = "item"
         const val SIMPLEHASH_EVENT_INCOMING = "simplehash_event_incoming"
-        const val SIMPLEHASH_META_CACHE = "simplehash_meta_cache"
+        const val SIMPLEHASH_META_ITEM = "simplehash_meta_item"
     }
 }
