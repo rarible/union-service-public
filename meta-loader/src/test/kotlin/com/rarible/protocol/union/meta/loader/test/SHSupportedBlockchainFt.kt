4e3697672507e1594c715fe7df1ea920ef31b352
Roman Novikov <mystdeim@gmail.com>
2023-08-21 13:33:44 +0100

    fix: ktlint

--- meta-loader/src/test/kotlin/com/rarible/protocol/union/meta/loader/test/SHSupportedBlockchainFt.kt
+++ meta-loader/src/test/kotlin/com/rarible/protocol/union/meta/loader/test/SHSupportedBlockchainFt.kt
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 
-
 @EnableAutoConfiguration
 @IntegrationTest
 @SpringBootTest(
