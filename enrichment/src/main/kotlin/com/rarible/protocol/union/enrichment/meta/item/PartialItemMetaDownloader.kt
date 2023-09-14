568f9442054042e0dd8ee6d807c1a2ba02512137
Artem Yakischik <a.yakischik@rarible.com>
2023-09-14 12:36:12 +0400

    Partial meta download fix

--- enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/meta/item/PartialItemMetaDownloader.kt
+++ enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/meta/item/PartialItemMetaDownloader.kt
@@ -26,7 +26,7 @@ class PartialItemMetaDownloader(
     private val itemRepository: ItemRepository,
 ) : Downloader<UnionMeta> {
 
-    override val type = "Item_Partial"
+    override val type = "Item"
 
     override suspend fun download(id: String): UnionMeta {
         val result = try {
