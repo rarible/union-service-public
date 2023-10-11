b0dc0cf64822b2d80934b8945382dfc08c9b1577
Artem Yakischik <a.yakischik@rarible.com>
2023-10-11 11:36:17 +0400

    feat: Signature API for input messages generation

--- core/src/main/kotlin/com/rarible/protocol/union/core/service/SignatureService.kt
+++ core/src/main/kotlin/com/rarible/protocol/union/core/service/SignatureService.kt
@@ -1,6 +1,7 @@
 package com.rarible.protocol.union.core.service
 
 import com.rarible.protocol.union.core.service.router.BlockchainService
+import com.rarible.protocol.union.dto.SignatureInputFormDto
 
 interface SignatureService : BlockchainService {
 
@@ -10,4 +11,7 @@ interface SignatureService : BlockchainService {
         signature: String,
         message: String
     ): Boolean
+
+    // TODO Ideally there should be intermediate object SignatureInputForm with 'native' values only
+    suspend fun getInput(form: SignatureInputFormDto): String
 }
--- integration/ethereum/src/test/kotlin/com/rarible/protocol/union/integration/ethereum/service/EthSignatureServiceTest.kt
+++ integration/ethereum/src/test/kotlin/com/rarible/protocol/union/integration/ethereum/service/EthSignatureServiceTest.kt
@@ -1,19 +1,30 @@
 package com.rarible.protocol.union.integration.ethereum.service
 
 import com.rarible.core.test.data.randomAddress
+import com.rarible.core.test.data.randomBigInt
 import com.rarible.core.test.data.randomBinary
 import com.rarible.core.test.data.randomString
 import com.rarible.protocol.dto.EthereumSignatureValidationFormDto
+import com.rarible.protocol.dto.SeaportFulfillmentSimpleResponseDto
+import com.rarible.protocol.dto.X2Y2GetCancelInputRequestDto
+import com.rarible.protocol.dto.X2Y2OrderSignRequestDto
+import com.rarible.protocol.dto.X2Y2SignResponseDto
 import com.rarible.protocol.order.api.client.OrderSignatureControllerApi
+import com.rarible.protocol.union.core.exception.UnionException
 import com.rarible.protocol.union.dto.BlockchainDto
+import com.rarible.protocol.union.dto.OpenSeaFillOrderSignatureInputFormDto
+import com.rarible.protocol.union.dto.X2Y2CancelOrderSignatureInputFormDto
+import com.rarible.protocol.union.dto.X2Y2FillOrderSignatureInputFormDto
 import com.rarible.protocol.union.integration.ethereum.converter.EthConverter
 import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import reactor.kotlin.core.publisher.toMono
 
@@ -29,7 +40,7 @@ class EthSignatureServiceTest {
     lateinit var service: EthSignatureService
 
     @Test
-    fun `ethereum validate`() = runBlocking<Unit> {
+    fun `validate - ok`() = runBlocking<Unit> {
         val expected = EthereumSignatureValidationFormDto(
             signer = randomAddress(),
             signature = randomBinary(),
@@ -46,5 +57,85 @@ class EthSignatureServiceTest {
         )
 
         assertThat(result).isEqualTo(true)
+    }
+
+    @Test
+    fun `get input - ok, opensea order fill input`() = runBlocking<Unit> {
+        val input = randomBinary()
+        val response = SeaportFulfillmentSimpleResponseDto(input)
+        val orderId = randomString()
+
+        every { signatureControllerApi.getSeaportOrderSignature(orderId) } returns response.toMono()
+
+        val result = service.getInput(OpenSeaFillOrderSignatureInputFormDto(BlockchainDto.ETHEREUM, orderId))
+
+        assertThat(result).isEqualTo(input.prefixed())
+    }
+
+    @Test
+    fun `get input - ok, x2y2 order fill input`() = runBlocking<Unit> {
+        val input = randomString()
+        val response = X2Y2SignResponseDto(input)
+        val request = X2Y2OrderSignRequestDto(
+            caller = randomString(),
+            op = randomBigInt(),
+            orderId = randomBigInt(),
+            currency = randomAddress(),
+            price = randomBigInt(),
+            tokenId = randomBigInt()
+        )
+
+        val form = X2Y2FillOrderSignatureInputFormDto(
+            blockchain = BlockchainDto.POLYGON,
+            caller = request.caller,
+            op = request.op,
+            orderId = request.orderId,
+            currency = request.currency.prefixed(),
+            price = request.price,
+            tokenId = request.tokenId
+        )
+
+        every { signatureControllerApi.orderSignX2Y2(request) } returns response.toMono()
+
+        val result = service.getInput(form)
+
+        assertThat(result).isEqualTo(input)
+    }
+
+    @Test
+    fun `get input - ok, x2y2 order cancel input`() = runBlocking<Unit> {
+        val input = randomString()
+        val response = X2Y2SignResponseDto(input)
+        val request = X2Y2GetCancelInputRequestDto(
+            caller = randomString(),
+            op = randomBigInt(),
+            orderId = randomBigInt(),
+            signMessage = randomString(),
+            sign = randomString(),
+        )
+
+        val form = X2Y2CancelOrderSignatureInputFormDto(
+            blockchain = BlockchainDto.POLYGON,
+            caller = request.caller,
+            op = request.op,
+            orderId = request.orderId,
+            signMessage = request.signMessage,
+            sign = request.sign,
+        )
+
+        every { signatureControllerApi.cancelSignX2Y2(request) } returns response.toMono()
+
+        val result = service.getInput(form)
+
+        assertThat(result).isEqualTo(input)
+    }
+
+    @Test
+    fun `get input - failed, unsupported blockchain`() = runBlocking<Unit> {
+        val orderId = randomString()
+
+        assertThrows<UnionException> {
+            service.getInput(OpenSeaFillOrderSignatureInputFormDto(BlockchainDto.MANTLE, orderId))
+        }
     }
 }
