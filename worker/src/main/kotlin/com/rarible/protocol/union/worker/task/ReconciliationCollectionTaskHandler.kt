db4f7dd4b0dc38dbf877c8f19a15055eb1596667
Roman Novikov <mystdeim@gmail.com>
2023-11-14 15:28:15 +0000

    fix: cr

--- worker/src/main/kotlin/com/rarible/protocol/union/worker/task/ReconciliationCollectionTaskHandler.kt
+++ worker/src/main/kotlin/com/rarible/protocol/union/worker/task/ReconciliationCollectionTaskHandler.kt
@@ -1,22 +1,15 @@
 package com.rarible.protocol.union.worker.task
 
-import com.rarible.core.task.RunTask
 import com.rarible.core.task.TaskHandler
-import com.rarible.protocol.union.core.service.router.ActiveBlockchainProvider
 import com.rarible.protocol.union.worker.job.ReconciliationCollectionJob
 import org.springframework.stereotype.Component
 
 @Component
 class ReconciliationCollectionTaskHandler(
-    private val job: ReconciliationCollectionJob,
-    private val activeBlockchainProvider: ActiveBlockchainProvider
+    private val job: ReconciliationCollectionJob
 ) : TaskHandler<String> {
 
     override val type = "ENRICHMENT_RECONCILIATION_COLLECTION_JOB"
-
-    override fun getAutorunParams(): List<RunTask> {
-        return activeBlockchainProvider.blockchains.map { RunTask(it.name) }
-    }
 
     override fun runLongTask(from: String?, param: String) = job.handle(from, param)
 }
