aa734fd7d0269ee6b402227aac3f064736e34527
sanufriev <s.anufriev@rarible.com>
2023-12-07 23:27:19 +0700

    feat: Make trait handle async

--- enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/meta/item/trait/TraitService.kt
+++ enrichment/src/main/kotlin/com/rarible/protocol/union/enrichment/meta/item/trait/TraitService.kt
@@ -1,5 +1,6 @@
 package com.rarible.protocol.union.enrichment.meta.item.trait
 
+import com.rarible.core.common.asyncBatchHandle
 import com.rarible.protocol.union.dto.CollectionIdDto
 import com.rarible.protocol.union.enrichment.model.ItemAttributeCountChange
 import com.rarible.protocol.union.enrichment.repository.TraitRepository
@@ -13,7 +14,7 @@ class TraitService(
         collectionId: CollectionIdDto,
         changes: Set<ItemAttributeCountChange>,
     ) {
-        changes.forEach {
+        changes.asyncBatchHandle(TRAIT_HANDLE_BATCH) {
             val traitId = traitRepository.incrementItemsCount(
                 collectionId,
                 it.attribute,
@@ -26,5 +27,10 @@ class TraitService(
 
     private suspend fun indexTrait(traitId: String) {
         // TODO: Index trait
+    }
+
+    private companion object {
+        // We don't expect that there will be more than 500 traits in one collection
+        const val TRAIT_HANDLE_BATCH = 500
     }
 }
